4261435b80cfbfd0a1cbb2a4c3ff16528db2e9aa
lajoie <lajoie@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2006-01-05 16:04:09 +0000

    Added AttributeProfile implementation and unit tests

---
+++ import/java-opensaml2/trunk/src/org/opensaml/saml2/metadata/impl/AttributeProfileUnmarshaller.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright [2005] [University Corporation for Advanced Internet Development, Inc.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * 
+ */
+
+package org.opensaml.saml2.metadata.impl;
+
+import org.opensaml.common.SAMLObject;
+import org.opensaml.common.impl.AbstractSAMLObjectUnmarshaller;
+import org.opensaml.common.impl.UnknownAttributeException;
+import org.opensaml.common.impl.UnknownElementException;
+import org.opensaml.common.xml.SAMLConstants;
+import org.opensaml.saml2.metadata.AttributeProfile;
+import org.opensaml.xml.io.UnmarshallingException;
+
+/**
+ * A thread safe {@link org.opensaml.common.io.Unmarshaller} for {@link org.opensaml.saml2.metadata.AttributeProfile}
+ * objects.
+ */
+public class AttributeProfileUnmarshaller extends AbstractSAMLObjectUnmarshaller {
+
+    /**
+     * Constructor
+     */
+    public AttributeProfileUnmarshaller() {
+        super(SAMLConstants.SAML20MD_NS, AttributeProfile.LOCAL_NAME);
+    }
+
+    /*
+     * @see org.opensaml.common.impl.AbstractSAMLObjectUnmarshaller#processChildElement(org.opensaml.common.SAMLObject,
+     *      org.opensaml.common.SAMLObject)
+     */
+    protected void processChildElement(SAMLObject parentElement, SAMLObject childElement)
+            throws UnmarshallingException, UnknownElementException {
+        // No Child Elements
+    }
+
+    /*
+     * @see org.opensaml.common.impl.AbstractSAMLObjectUnmarshaller#processAttribute(org.opensaml.common.SAMLObject,
+     *      java.lang.String, java.lang.String)
+     */
+    protected void processAttribute(SAMLObject samlElement, String attributeName, String attributeValue)
+            throws UnmarshallingException, UnknownAttributeException {
+        // No attributes
+    }
+
+    /*
+     * @see org.opensaml.common.impl.AbstractSAMLObjectUnmarshaller#unmarshallElementContent(org.opensaml.common.SAMLObject,
+     *      java.lang.String)
+     */
+    protected void unmarshallElementContent(SAMLObject samlElement, String elementContent) {
+        ((AttributeProfile) samlElement).setProfileURI(elementContent);
+    }
+}
