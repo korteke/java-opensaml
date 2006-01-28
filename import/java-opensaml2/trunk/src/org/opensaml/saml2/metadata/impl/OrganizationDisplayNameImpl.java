20a452113e7fe63a083a923f6b266777c3a8902c
lajoie <lajoie@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2006-01-28 01:04:53 +0000

    OrganizationURL impl and test

---
+++ import/java-opensaml2/trunk/src/org/opensaml/saml2/metadata/impl/OrganizationDisplayNameImpl.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright [2005] [University Corporation for Advanced Internet Development, Inc.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * 
+ */
+
+package org.opensaml.saml2.metadata.impl;
+
+import java.util.List;
+
+import org.opensaml.common.SAMLObject;
+import org.opensaml.common.impl.AbstractSAMLObject;
+import org.opensaml.common.xml.SAMLConstants;
+import org.opensaml.saml2.metadata.OrganizationDisplayName;
+
+/**
+ * Concrete implementation of {@link org.opensaml.saml2.metadata.OrganizationDisplayName}
+ */
+public class OrganizationDisplayNameImpl extends AbstractSAMLObject implements OrganizationDisplayName {
+
+    /** Organization name */
+    private String name;
+
+    /**
+     * Constructor
+     */
+    public OrganizationDisplayNameImpl() {
+        super(SAMLConstants.SAML20MD_NS, OrganizationDisplayName.LOCAL_NAME);
+        setElementNamespacePrefix(SAMLConstants.SAML20MD_PREFIX);
+    }
+
+    /*
+     * @see org.opensaml.saml2.metadata.OrganizationDisplayName#getName()
+     */
+    public String getName() {
+        return name;
+    }
+
+    /*
+     * @see org.opensaml.saml2.metadata.OrganizationDisplayName#setName(java.lang.String)
+     */
+    public void setName(String newName) {
+        name = prepareForAssignment(name, newName);
+    }
+
+    /*
+     * @see org.opensaml.common.SAMLObject#getOrderedChildren()
+     */
+    public List<SAMLObject> getOrderedChildren() {
+        return null;
+    }
+}
---
+++ import/java-opensaml2/trunk/test/org/opensaml/saml2/metadata/impl/OrganizationURLTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright [2005] [University Corporation for Advanced Internet Development, Inc.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * 
+ */
+package org.opensaml.saml2.metadata.impl;
+
+import javax.xml.namespace.QName;
+
+import org.opensaml.common.SAMLObjectBaseTestCase;
+import org.opensaml.common.xml.SAMLConstants;
+import org.opensaml.saml2.metadata.OrganizationURL;
+
+/**
+ * Test case for creating, marshalling, and unmarshalling
+ * {@link org.opensaml.saml2.metadata.OrganizationURL}.
+ */
+public class OrganizationURLTest extends SAMLObjectBaseTestCase {
+    
+    /** Expected URL */
+    protected String expectURL;
+    
+    /**
+     * Constructor
+     */
+    public OrganizationURLTest() {
+        singleElementFile = "/data/org/opensaml/saml2/metadata/impl/OrganizationURL.xml";
+    }
+    
+    /*
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        
+        expectURL = "http://example.org";
+    }
+
+    /*
+     * @see org.opensaml.common.SAMLObjectBaseTestCase#testSingleElementUnmarshall()
+     */
+    public void testSingleElementUnmarshall() {
+        OrganizationURL url = (OrganizationURL) unmarshallElement(singleElementFile);
+        
+        assertEquals("URL was not expected value", expectURL, url.getURL());
+    }
+
+    /*
+     * @see org.opensaml.common.SAMLObjectBaseTestCase#testSingleElementOptionalAttributesUnmarshall()
+     */
+    public void testSingleElementOptionalAttributesUnmarshall() {
+        // No optional attributes
+    }
+
+    /*
+     * @see org.opensaml.common.SAMLObjectBaseTestCase#testSingleElementMarshall()
+     */
+    public void testSingleElementMarshall() {
+        QName qname = new QName(SAMLConstants.SAML20MD_NS, OrganizationURL.LOCAL_NAME);
+        OrganizationURL url = (OrganizationURL) buildSAMLObject(qname);
+        
+        url.setURL(expectURL);
+
+        assertEquals(expectedDOM, url);
+    }
+
+    /*
+     * @see org.opensaml.common.SAMLObjectBaseTestCase#testSingleElementOptionalAttributesMarshall()
+     */
+    public void testSingleElementOptionalAttributesMarshall() {
+        // No optional attributes
+    }
+}
