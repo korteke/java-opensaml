9408ee47c1706ad3146caeca2c1090f75ce838d9
putmanb <putmanb@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2006-02-08 16:05:29 +0000

    Some Core interfaces

---
+++ import/java-opensaml2/trunk/src/org/opensaml/saml2/core/StatusResponse.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright [2005] [University Corporation for Advanced Internet Development, Inc.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * 
+ */
+package org.opensaml.saml2.core;
+
+import org.joda.time.DateTime;
+import org.opensaml.common.SAMLObject;
+
+/**
+ * SAML 2.0 Core StatusResponseType.  
+ */
+public interface StatusResponse extends SAMLObject {
+    
+    /* Local name */
+    public static final String LOCAL_NAME = "StatusResponse";
+    
+    /* InResponseTo attribute name */
+    public static final String IN_RESPONSE_TO_ATTRIB_NAME = "InResponseTo";
+    
+    /* Version attribute name */
+    public static final String VERSION_ATTRIB_NAME = "Version";
+    
+    /* IssueInstant attribute name */
+    public static final String ISSUE_INSTANT_ATTRIB_NAME = "IssueInstant";
+    
+    /* Destination attribute name */
+    public static final String DESTINATION_ATTRIB_NAME = "Destination";
+    
+    /* Consent attribute name */
+    public static final String CONSENT_ATTRIB_NAME = "Consent";
+
+    /**
+     * Gets the date/time the response was issued.
+     * 
+     * @return the date/time the response was issued
+     */
+    public DateTime getIssueInstant();
+    
+    /**
+     * Sets the date/time the response was issued.
+     * 
+     * param newIssueInstant the date/time the response was issued
+     */
+    public void setIssueInstant(DateTime newIssueInstant);
+    
+    /**
+     * Gets the URI of the destination of the response.
+     * 
+     * @return the URI of the destination of the response
+     */
+    public String getDestination();
+    
+    /**
+     * Sets the URI of the destination of the response.
+     * 
+     * @param newDestination the URI of the destination of the response
+     */
+    public void setDestination(String newDestination);
+    
+    /**
+     * Gets the consent obtained from the principal for sending this response.
+     * 
+     * @return the consent obtained from the principal for sending this response
+     */
+    public String getConsent();
+    
+    /**
+     * Sets the consent obtained from the principal for sending this response.
+     * 
+     * @param consent the consent obtained from the principal for sending this response
+     */
+    public void setConsent(String consent);
+    
+    /**
+     * Gets the issuer of this response.
+     * 
+     * @return the issuer of this response
+     */
+    public Issuer getIssuer();
+    
+    /**
+     * Sets the issuer of this response.
+     * 
+     * @param newIssuer the issuer of this response
+     */
+    public void setIssuer(Issuer newIssuer);
+    
+    /**
+     * Gets the Status of this response.
+     * 
+     * @return the Status of this response
+     */
+    public Status getStatus();
+    
+    /**
+     * Sets the Status of this response.
+     * 
+     * @param newStatus the Status of this response
+     */
+    public void setStatus(Status newStatus);
+    
+    //TODO Extensions
+    
+}
