236504e92b7dbb36d3775f522f35e5faefa6e88f
knm6 <knm6@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2006-02-16 23:22:29 +0000

    Added builder, implementation, marshaller, and unmarshaller for Action

---
+++ import/java-opensaml2/trunk/src/org/opensaml/saml2/core/impl/ActionImpl.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright [2005] [University Corporation for Advanced Internet Development, Inc.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * 
+ */
+
+package org.opensaml.saml2.core.impl;
+
+import java.util.List;
+
+import org.opensaml.common.SAMLObject;
+import org.opensaml.common.impl.AbstractSAMLObject;
+import org.opensaml.common.xml.SAMLConstants;
+import org.opensaml.saml2.core.Action;
+
+/**
+ * A concrete implementation of {@link org.opensaml.saml2.core.Action}.
+ */
+public class ActionImpl extends AbstractSAMLObject implements Action {
+
+    /** URI of the Namespace of this Action */
+    private String namespace;
+
+    /** Action value */
+    private String action;
+
+    /** Constructor */
+    public ActionImpl() {
+        super(SAMLConstants.SAML20_NS, Action.LOCAL_NAME);
+        setElementNamespacePrefix(SAMLConstants.SAML20_PREFIX);
+    }
+
+    /*
+     * @see org.opensaml.saml2.core.Action#getNamespace()
+     */
+    public String getNamespace() {
+        return namespace;
+    }
+
+    /*
+     * @see org.opensaml.saml2.core.Action#setNamespace(java.lang.String)
+     */
+    public void setNamespace(String newNamespace) {
+        this.namespace = prepareForAssignment(this.namespace, newNamespace);
+    }
+
+    /*
+     * @see org.opensaml.saml2.core.Action#getAction()
+     */
+    public String getAction() {
+        return action;
+    }
+
+    /*
+     * @see org.opensaml.saml2.core.Action#setAction(java.lang.String)
+     */
+    public void setAction(String newAction) {
+        this.action = prepareForAssignment(this.action, newAction);
+    }
+
+    /*
+     * @see org.opensaml.common.SAMLObject#getOrderedChildren()
+     */
+    public List<SAMLObject> getOrderedChildren() {
+        return null;
+    }
+}
