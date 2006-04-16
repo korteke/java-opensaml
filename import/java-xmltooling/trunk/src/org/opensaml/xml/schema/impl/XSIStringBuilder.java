7cebe10659da3bcbc068897fa814b8d20f796c6a
lajoie <lajoie@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2006-04-16 12:42:41 +0000

    Added XSI String object provider

---
+++ import/java-xmltooling/trunk/src/org/opensaml/xml/schema/impl/XSIStringBuilder.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright [2006] [University Corporation for Advanced Internet Development, Inc.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.xml.schema.impl;
+
+import org.opensaml.xml.AbstractXMLObjectBuilder;
+import org.opensaml.xml.XMLObject;
+
+/**
+ * Builder of {@link org.opensaml.xml.schema.impl.XSIStringImpl} objects.
+ */
+public class XSIStringBuilder extends AbstractXMLObjectBuilder {
+
+    /**
+     * {@inheritDoc}
+     */
+    public XMLObject buildObject(String namespaceURI, String localName, String namespacePrefix) {
+        return new XSIStringImpl(namespaceURI, localName, namespacePrefix);
+    }
+}
