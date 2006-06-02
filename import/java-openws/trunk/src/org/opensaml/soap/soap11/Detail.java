4f801aa476ab339b2731a2c83a8c26832aaff424
lajoie <lajoie@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2006-06-02 14:13:48 +0000

    Initial import

---
+++ import/java-openws/trunk/src/org/opensaml/soap/soap11/Detail.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright [2006] [University Corporation for Advanced Internet Development, Inc.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.soap.soap11;
+
+import javax.xml.namespace.QName;
+
+import org.opensaml.soap.util.SOAPConstants;
+import org.opensaml.xml.AttributeExtensibleXMLObject;
+import org.opensaml.xml.ElementExtensibleXMLObject;
+import org.opensaml.xml.validation.ValidatingXMLObject;
+
+/**
+ * SOAP 1.1 Detail
+ */
+public interface Detail extends ValidatingXMLObject, ElementExtensibleXMLObject, AttributeExtensibleXMLObject {
+
+    /** Element local name */
+    public final static String DEFAULT_ELEMENT_LOCAL_NAME = "detail";
+    
+    /** Default element name */
+    public final static QName DEFAULT_ELEMENT_NAME = new QName(SOAPConstants.SOAP11_NS, DEFAULT_ELEMENT_LOCAL_NAME, SOAPConstants.SOAP11_PREFIX);
+    
+    /** Local name of the XSI type */
+    public final static String TYPE_LOCAL_NAME = "detail"; 
+        
+    /** QName of the XSI type */
+    public final static QName TYPE_NAME = new QName(SOAPConstants.SOAP11_NS, TYPE_LOCAL_NAME, SOAPConstants.SOAP11_PREFIX);
+}
---
+++ import/java-openws/trunk/src/org/opensaml/soap/soap11/Envelope.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright [2006] [University Corporation for Advanced Internet Development, Inc.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.soap.soap11;
+
+import javax.xml.namespace.QName;
+
+import org.opensaml.soap.util.SOAPConstants;
+import org.opensaml.xml.AttributeExtensibleXMLObject;
+import org.opensaml.xml.ElementExtensibleXMLObject;
+import org.opensaml.xml.validation.ValidatingXMLObject;
+
+/**
+ * SOAP 1.1 Envelope
+ */
+public interface Envelope extends ValidatingXMLObject, ElementExtensibleXMLObject, AttributeExtensibleXMLObject {
+
+    /** Element local name */
+    public final static String DEFAULT_ELEMENT_LOCAL_NAME = "Envelope";
+    
+    /** Default element name */
+    public final static QName DEFAULT_ELEMENT_NAME = new QName(SOAPConstants.SOAP11_NS, DEFAULT_ELEMENT_LOCAL_NAME, SOAPConstants.SOAP11_PREFIX);
+    
+    /** Local name of the XSI type */
+    public final static String TYPE_LOCAL_NAME = "Envelope"; 
+        
+    /** QName of the XSI type */
+    public final static QName TYPE_NAME = new QName(SOAPConstants.SOAP11_NS, TYPE_LOCAL_NAME, SOAPConstants.SOAP11_PREFIX);
+    
+    /**
+     * Gets the header of this envelope.
+     * 
+     * @return the header of this envelope
+     */
+    public Header getHeader();
+    
+    /**
+     * Sets the header of this envelope.
+     * 
+     * @param newHeader the header of this envelope
+     */
+    public void setHeader(Header newHeader);
+    
+    /**
+     * Gets the body of this envelope.
+     * 
+     * @return the body of this envelope
+     */
+    public Body getBody();
+    
+    /**
+     * Sets the body of this envelope.
+     * 
+     * @param newBody the body of this envelope
+     */
+    public void setBody(Body newBody);
+}
