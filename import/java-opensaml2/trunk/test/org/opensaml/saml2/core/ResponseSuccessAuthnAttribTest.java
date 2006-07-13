d8aac9a56130a85df49cae500e1c03cd2d728e45
lajoie <lajoie@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2006-07-13 20:20:31 +0000

    Fixed unit test

--- import/java-opensaml2/trunk/test/org/opensaml/saml2/core/ResponseSuccessAuthnAttribTest.java
+++ import/java-opensaml2/trunk/test/org/opensaml/saml2/core/ResponseSuccessAuthnAttribTest.java
@@ -21,7 +21,10 @@ import org.joda.time.chrono.ISOChronology;
 import org.opensaml.common.ComplexSAMLObjectBaseTestCase;
 import org.opensaml.common.SAMLVersion;
 import org.opensaml.xml.XMLObjectBuilder;
+import org.opensaml.xml.io.Marshaller;
 import org.opensaml.xml.schema.XSString;
+import org.opensaml.xml.util.XMLHelper;
+import org.w3c.dom.Element;
 
 /**
  * Tests unmarshalling and marshalling for various response messages.
@@ -95,98 +98,106 @@ public class ResponseSuccessAuthnAttribTest extends ComplexSAMLObjectBaseTestCas
     /*
      * @see org.opensaml.common.ComplexSAMLObjectBaseTestCase#testMarshall()
      */
-    public void testMarshall() {
-//        Response response = (Response) buildXMLObject(Response.DEFAULT_ELEMENT_NAME);
-//        response.setID("_c7055387-af61-4fce-8b98-e2927324b306");
-//        response.setInResponseTo("_abcdef123456");
-//        response.setIssueInstant(new DateTime(2006, 1, 26, 13, 35, 5, 0, ISOChronology.getInstanceUTC()));
-//        
-//        Issuer rIssuer = (Issuer) buildXMLObject(Issuer.DEFAULT_ELEMENT_NAME);
-//        rIssuer.setFormat("urn:oasis:names:tc:SAML:2.0:nameid-format:entity");
-//        rIssuer.setValue("https://idp.example.org");
-//        
-//        Status status = (Status) buildXMLObject(Status.DEFAULT_ELEMENT_NAME);
-//        StatusCode statusCode = (StatusCode) buildXMLObject(StatusCode.DEFAULT_ELEMENT_NAME);
-//        statusCode.setValue("urn:oasis:names:tc:SAML:2.0:status:Success");
-//        
-//        Assertion assertion = (Assertion) buildXMLObject(Assertion.DEFAULT_ELEMENT_NAME);
-//        assertion.setID("_a75adf55-01d7-40cc-929f-dbd8372ebdfc");
-//        assertion.setIssueInstant(new DateTime(2006, 1, 26, 13, 35, 5, 0, ISOChronology.getInstanceUTC()));
-//        
-//        Issuer aIssuer = (Issuer) buildXMLObject(Issuer.DEFAULT_ELEMENT_NAME);
-//        aIssuer.setFormat("urn:oasis:names:tc:SAML:2.0:nameid-format:entity");
-//        aIssuer.setValue("https://idp.example.org");
-//        
-//        Subject subject = (Subject) buildXMLObject(Subject.DEFAULT_ELEMENT_NAME);
-//        NameID nameID = (NameID) buildXMLObject(NameID.DEFAULT_ELEMENT_NAME);
-//        nameID.setFormat("urn:oasis:names:tc:SAML:2.0:nameid-format:transient");
-//        nameID.setValue("_820d2843-2342-8236-ad28-8ac94fb3e6a1");
-//        
-//        SubjectConfirmation subjectConfirmation = (SubjectConfirmation) buildXMLObject(SubjectConfirmation.DEFAULT_ELEMENT_NAME);
-//        subjectConfirmation.setMethod("urn:oasis:names:tc:SAML:2.0:cm:bearer");
-//        
-//        Conditions conditions = (Conditions) buildXMLObject(Conditions.DEFAULT_ELEMENT_NAME);
-//        conditions.setNotBefore(new DateTime(2006, 1, 26, 13, 35, 5, 0, ISOChronology.getInstanceUTC()));
-//        conditions.setNotOnOrAfter(new DateTime(2006, 1, 26, 13, 45, 5, 0, ISOChronology.getInstanceUTC()));
-//        
-//        AudienceRestriction audienceRestriction = (AudienceRestriction) buildXMLObject(AudienceRestriction.DEFAULT_ELEMENT_NAME);
-//        Audience audience = (Audience) buildXMLObject(Audience.DEFAULT_ELEMENT_NAME);
-//        audience.setAudienceURI("https://sp.example.org");
-//        
-//        AuthnStatement authnStatement = (AuthnStatement) buildXMLObject(AuthnStatement.DEFAULT_ELEMENT_NAME);
-//        authnStatement.setAuthnInstant(new DateTime(2006, 1, 26, 13, 35, 5, 0, ISOChronology.getInstanceUTC()));
-//        
-//        AuthnContext authnContext = (AuthnContext) buildXMLObject(AuthnContext.DEFAULT_ELEMENT_NAME);
-//        AuthnContextClassRef classRef = (AuthnContextClassRef) buildXMLObject(AuthnContextClassRef.DEFAULT_ELEMENT_NAME);
-//        classRef.setAuthnContextClassRef("urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport");
-//        
-//        AttributeStatement attribStatement = (AttributeStatement) buildXMLObject(AttributeStatement.DEFAULT_ELEMENT_NAME);
-//        XMLObjectBuilder stringBuilder = builderFactory.getBuilder(XSString.TYPE_NAME);
-//        
-//        Attribute fooAttrib = (Attribute) buildXMLObject(Attribute.DEFAULT_ELEMENT_NAME);
-//        fooAttrib.setFriendlyName("fooAttrib");
-//        fooAttrib.setName("urn:foo:attrib");
-//        fooAttrib.setNameFormat("urn:oasis:names:tc:SAML:2.0:attrname-format:uri");
-//        XSString fooAttribValue = null;
-//        fooAttribValue = (XSString) stringBuilder.buildObject(AttributeValue.DEFAULT_ELEMENT_NAME, XSString.TYPE_NAME);
-//        fooAttribValue.setValue("SomeValue");
-//        fooAttrib.getAttributeValues().add(fooAttribValue);
-//        fooAttribValue = (XSString) stringBuilder.buildObject(AttributeValue.DEFAULT_ELEMENT_NAME, XSString.TYPE_NAME);
-//        fooAttribValue.setValue("SomeOtherValue");
-//        fooAttrib.getAttributeValues().add(fooAttribValue);
-//        
-//        Attribute ldapAttrib = (Attribute) buildXMLObject(Attribute.DEFAULT_ELEMENT_NAME);
-//        ldapAttrib.setFriendlyName("eduPersonPrincipalName");
-//        ldapAttrib.setName("urn:oid:1.3.6.1.4.1.5923.1.1.1.6");
-//        ldapAttrib.setNameFormat("urn:oasis:names:tc:SAML:2.0:attrname-format:uri");
-//        XSString ldapAttribValue = (XSString) stringBuilder.buildObject(AttributeValue.DEFAULT_ELEMENT_NAME, XSString.TYPE_NAME);
-//        //TODO how to handle the x500:Encoding attribute on AttributeValue ?
-//        ldapAttribValue.setValue("j.doe@idp.example.org");
-//        ldapAttrib.getAttributeValues().add(ldapAttribValue);
-//        
-//        response.setIssuer(rIssuer);
-//        status.setStatusCode(statusCode);
-//        response.setStatus(status);
-//        response.getAssertions().add(assertion);
-//        
-//        assertion.setIssuer(aIssuer);
-//        subject.setNameID(nameID);
-//        subject.getSubjectConfirmations().add(subjectConfirmation);
-//        assertion.setSubject(subject);
-//        
-//        audienceRestriction.getAudiences().add(audience);
-//        conditions.getAudienceRestrictions().add(audienceRestriction);
-//        assertion.setConditions(conditions);
-//        
-//        authnContext.setAuthnContextClassRef(classRef);
-//        authnStatement.setAuthnContext(authnContext);
-//        assertion.getAuthnStatements().add(authnStatement);
-//        
-//        attribStatement.getAttributes().add(fooAttrib);
-//        attribStatement.getAttributes().add(ldapAttrib);
-//        assertion.getAttributeStatement().add(attribStatement);
-//
-//        assertEquals("Marshalled Response was not the expected value", expectedDOM, response);
+    public void testMarshall(){
+        Response response = (Response) buildXMLObject(Response.DEFAULT_ELEMENT_NAME);
+        response.setID("_c7055387-af61-4fce-8b98-e2927324b306");
+        response.setInResponseTo("_abcdef123456");
+        response.setIssueInstant(new DateTime(2006, 1, 26, 13, 35, 5, 0, ISOChronology.getInstanceUTC()));
+        
+        Issuer rIssuer = (Issuer) buildXMLObject(Issuer.DEFAULT_ELEMENT_NAME);
+        rIssuer.setFormat("urn:oasis:names:tc:SAML:2.0:nameid-format:entity");
+        rIssuer.setValue("https://idp.example.org");
+        
+        Status status = (Status) buildXMLObject(Status.DEFAULT_ELEMENT_NAME);
+        StatusCode statusCode = (StatusCode) buildXMLObject(StatusCode.DEFAULT_ELEMENT_NAME);
+        statusCode.setValue("urn:oasis:names:tc:SAML:2.0:status:Success");
+        
+        Assertion assertion = (Assertion) buildXMLObject(Assertion.DEFAULT_ELEMENT_NAME);
+        assertion.setID("_a75adf55-01d7-40cc-929f-dbd8372ebdfc");
+        assertion.setIssueInstant(new DateTime(2006, 1, 26, 13, 35, 5, 0, ISOChronology.getInstanceUTC()));
+        
+        Issuer aIssuer = (Issuer) buildXMLObject(Issuer.DEFAULT_ELEMENT_NAME);
+        aIssuer.setFormat("urn:oasis:names:tc:SAML:2.0:nameid-format:entity");
+        aIssuer.setValue("https://idp.example.org");
+        
+        Subject subject = (Subject) buildXMLObject(Subject.DEFAULT_ELEMENT_NAME);
+        NameID nameID = (NameID) buildXMLObject(NameID.DEFAULT_ELEMENT_NAME);
+        nameID.setFormat("urn:oasis:names:tc:SAML:2.0:nameid-format:transient");
+        nameID.setValue("_820d2843-2342-8236-ad28-8ac94fb3e6a1");
+        
+        SubjectConfirmation subjectConfirmation = (SubjectConfirmation) buildXMLObject(SubjectConfirmation.DEFAULT_ELEMENT_NAME);
+        subjectConfirmation.setMethod("urn:oasis:names:tc:SAML:2.0:cm:bearer");
+        
+        Conditions conditions = (Conditions) buildXMLObject(Conditions.DEFAULT_ELEMENT_NAME);
+        conditions.setNotBefore(new DateTime(2006, 1, 26, 13, 35, 5, 0, ISOChronology.getInstanceUTC()));
+        conditions.setNotOnOrAfter(new DateTime(2006, 1, 26, 13, 45, 5, 0, ISOChronology.getInstanceUTC()));
+        
+        AudienceRestriction audienceRestriction = (AudienceRestriction) buildXMLObject(AudienceRestriction.DEFAULT_ELEMENT_NAME);
+        Audience audience = (Audience) buildXMLObject(Audience.DEFAULT_ELEMENT_NAME);
+        audience.setAudienceURI("https://sp.example.org");
+        
+        AuthnStatement authnStatement = (AuthnStatement) buildXMLObject(AuthnStatement.DEFAULT_ELEMENT_NAME);
+        authnStatement.setAuthnInstant(new DateTime(2006, 1, 26, 13, 35, 5, 0, ISOChronology.getInstanceUTC()));
+        
+        AuthnContext authnContext = (AuthnContext) buildXMLObject(AuthnContext.DEFAULT_ELEMENT_NAME);
+        AuthnContextClassRef classRef = (AuthnContextClassRef) buildXMLObject(AuthnContextClassRef.DEFAULT_ELEMENT_NAME);
+        classRef.setAuthnContextClassRef("urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport");
+        
+        AttributeStatement attribStatement = (AttributeStatement) buildXMLObject(AttributeStatement.DEFAULT_ELEMENT_NAME);
+        XMLObjectBuilder stringBuilder = builderFactory.getBuilder(XSString.TYPE_NAME);
+        
+        Attribute fooAttrib = (Attribute) buildXMLObject(Attribute.DEFAULT_ELEMENT_NAME);
+        fooAttrib.setFriendlyName("fooAttrib");
+        fooAttrib.setName("urn:foo:attrib");
+        fooAttrib.setNameFormat("urn:oasis:names:tc:SAML:2.0:attrname-format:uri");
+        XSString fooAttribValue = null;
+        fooAttribValue = (XSString) stringBuilder.buildObject(AttributeValue.DEFAULT_ELEMENT_NAME, XSString.TYPE_NAME);
+        fooAttribValue.setValue("SomeValue");
+        fooAttrib.getAttributeValues().add(fooAttribValue);
+        fooAttribValue = (XSString) stringBuilder.buildObject(AttributeValue.DEFAULT_ELEMENT_NAME, XSString.TYPE_NAME);
+        fooAttribValue.setValue("SomeOtherValue");
+        fooAttrib.getAttributeValues().add(fooAttribValue);
+        
+        Attribute ldapAttrib = (Attribute) buildXMLObject(Attribute.DEFAULT_ELEMENT_NAME);
+        ldapAttrib.setFriendlyName("eduPersonPrincipalName");
+        ldapAttrib.setName("urn:oid:1.3.6.1.4.1.5923.1.1.1.6");
+        ldapAttrib.setNameFormat("urn:oasis:names:tc:SAML:2.0:attrname-format:uri");
+        XSString ldapAttribValue = (XSString) stringBuilder.buildObject(AttributeValue.DEFAULT_ELEMENT_NAME, XSString.TYPE_NAME);
+        //TODO how to handle the x500:Encoding attribute on AttributeValue ?
+        ldapAttribValue.setValue("j.doe@idp.example.org");
+        ldapAttrib.getAttributeValues().add(ldapAttribValue);
+        
+        response.setIssuer(rIssuer);
+        status.setStatusCode(statusCode);
+        response.setStatus(status);
+        response.getAssertions().add(assertion);
+        
+        assertion.setIssuer(aIssuer);
+        subject.setNameID(nameID);
+        subject.getSubjectConfirmations().add(subjectConfirmation);
+        assertion.setSubject(subject);
+        
+        audienceRestriction.getAudiences().add(audience);
+        conditions.getAudienceRestrictions().add(audienceRestriction);
+        assertion.setConditions(conditions);
+        
+        authnContext.setAuthnContextClassRef(classRef);
+        authnStatement.setAuthnContext(authnContext);
+        assertion.getAuthnStatements().add(authnStatement);
+        
+        attribStatement.getAttributes().add(fooAttrib);
+        attribStatement.getAttributes().add(ldapAttrib);
+        assertion.getAttributeStatement().add(attribStatement);
+        
+        try{
+        Marshaller marshaller = marshallerFactory.getMarshaller(response);
+        Element result = marshaller.marshall(response);
+        System.out.println(XMLHelper.nodeToString(result));
+        }catch(Exception e){
+            
+        }
+
+        assertEquals("Marshalled Response was not the expected value", expectedDOM, response);
     }
     
 }
