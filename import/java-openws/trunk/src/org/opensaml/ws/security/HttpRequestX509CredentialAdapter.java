e41883e2c72bb0f22a6e1b0ad8e55f796350b7a6
lajoie <lajoie@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2007-03-01 15:03:16 +0000

    Clean up impl

--- import/java-openws/trunk/src/org/opensaml/ws/security/HttpRequestX509CredentialAdapter.java
+++ import/java-openws/trunk/src/org/opensaml/ws/security/HttpRequestX509CredentialAdapter.java
@@ -16,103 +16,38 @@
 
 package org.opensaml.ws.security;
 
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.cert.X509CRL;
 import java.security.cert.X509Certificate;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
 
-import javax.crypto.SecretKey;
 import javax.servlet.http.HttpServletRequest;
 
 import org.opensaml.xml.security.credential.UsageType;
+import org.opensaml.xml.security.x509.BasicX509Credential;
 import org.opensaml.xml.security.x509.X509Credential;
-import org.opensaml.xml.util.DatatypeHelper;
 
 /**
  * An adapter that exposes the X.509 certificates contained in the HTTP request attribute as an
  * {@link X509Credentiall}.
  */
-public class HttpRequestX509CredentialAdapter implements X509Credential {
+public class HttpRequestX509CredentialAdapter extends BasicX509Credential implements X509Credential {
 
     /** HTTP header to pull certificate info from. */
     public static final String X509_CERT_HTTP_HEADER = "javax.servlet.request.X509Certificate";
 
-    /** ID of the peer. */
-    private String entityId;
-
-    /** Certificate chain for the entity, entity certificate is first element. */
-    private List<X509Certificate> certChain;
-
     /**
      * Constructor.
-     * 
-     * @param entity entity that issued the request
+     *
      * @param httpRequest the HTTP request
-     * 
-     * @throws IllegalArgumentException thrown if the entity is null or empty or the request does not contain any
-     *             certificates
      */
-    public HttpRequestX509CredentialAdapter(String entity, HttpServletRequest httpRequest)
-            throws IllegalArgumentException {
-        if (DatatypeHelper.isEmpty(entity)) {
-            throw new IllegalArgumentException("Entity ID may not be null or empty");
-        }
-
+    public HttpRequestX509CredentialAdapter(HttpServletRequest httpRequest) {
         X509Certificate[] chain = (X509Certificate[]) httpRequest.getAttribute(X509_CERT_HTTP_HEADER);
         if (chain == null | chain.length == 0) {
             throw new IllegalArgumentException("HTTP Request does not contain X.509 certificates in header "
                     + X509_CERT_HTTP_HEADER);
         }
 
-        entityId = DatatypeHelper.safeTrim(entity);
+        entityCert = chain[0];
         certChain = Arrays.asList(chain);
-    }
-
-    /** {@inheritDoc} */
-    public Collection<X509CRL> getCRLs() {
-        return null;
-    }
-
-    /** {@inheritDoc} */
-    public X509Certificate getEntityCertificate() {
-        return certChain.get(0);
-    }
-
-    /** {@inheritDoc} */
-    public Collection<X509Certificate> getEntityCertificateChain() {
-        return certChain;
-    }
-
-    /** {@inheritDoc} */
-    public String getEntityId() {
-        return entityId;
-    }
-
-    /** {@inheritDoc} */
-    public Collection<String> getKeyNames() {
-        return null;
-    }
-
-    /** {@inheritDoc} */
-    public PrivateKey getPrivateKey() {
-        return null;
-    }
-    
-    /** {@inheritDoc} */
-    public SecretKey getSecretyKey() {
-        return null;
-    }
-
-    /** {@inheritDoc} */
-    public Collection<PublicKey> getPublicKeys() {
-        return null;
-    }
-
-    /** {@inheritDoc} */
-    public UsageType getUsageType() {
-        return UsageType.SIGNING;
+        usageType = UsageType.SIGNING;
     }
 }
