f534bf18036ddf9b6f469565f6646fea0a0497a3
putmanb <putmanb@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2007-04-07 22:30:59 +0000

    Add direct access to the list of samlmd:EncryptionMethod children of a samlmd:KeyDescriptor in the SAMLMDCredentialContext. Components using the credential to which this context is attached could for example use to determine which encryption key transport algorithms the peer supports.

--- import/java-opensaml2/trunk/src/org/opensaml/security/SAMLMDCredentialContext.java
+++ import/java-opensaml2/trunk/src/org/opensaml/security/SAMLMDCredentialContext.java
@@ -16,6 +16,10 @@
 
 package org.opensaml.security;
 
+import java.util.List;
+
+import org.opensaml.saml2.metadata.EncryptionMethod;
+import org.opensaml.saml2.metadata.KeyDescriptor;
 import org.opensaml.saml2.metadata.RoleDescriptor;
 import org.opensaml.xml.security.keyinfo.KeyInfoCredentialContext;
 import org.opensaml.xml.signature.KeyInfo;
@@ -28,13 +32,27 @@ public class SAMLMDCredentialContext extends KeyInfoCredentialContext {
     /** Role containing the KeyInfo resolution context. */
     private RoleDescriptor role;
     
+    /** Role containing the KeyInfo resolution context. */
+    private List<EncryptionMethod> encMethods;
+    
     /** {@inheritDoc} */
     public void setKeyInfo(KeyInfo info) {
         super.setKeyInfo(info);
-        // KeyInfo -> KeyDescriptor -> RoleDescriptor
         if (info != null) {
+            // KeyInfo -> KeyDescriptor / EncryptionMethod
+            encMethods = ((KeyDescriptor) info.getParent()).getEncryptionMethods();
+            // KeyInfo -> KeyDescriptor -> RoleDescriptor
             role = (RoleDescriptor) info.getParent().getParent();
         }
+    }
+    
+    /**
+     * Return the list of {@link EncryptionMethod}'s associated with credential context
+     * 
+     * @return a list of SAML metadata encryption method associated with this context
+     */
+    public List<EncryptionMethod> getEncryptionMethod() {
+        return encMethods;
     }
     
     /**
