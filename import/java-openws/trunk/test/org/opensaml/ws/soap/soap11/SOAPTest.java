05efe636d84e5ed0e83daed1799b168150248eaa
putmanb <putmanb@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2008-02-12 01:00:37 +0000

    Minor fixups and additions to unit test.

--- import/java-openws/trunk/test/org/opensaml/ws/soap/soap11/SOAPTest.java
+++ import/java-openws/trunk/test/org/opensaml/ws/soap/soap11/SOAPTest.java
@@ -20,9 +20,11 @@ import javax.xml.namespace.QName;
 
 import org.opensaml.ws.BaseTestCase;
 import org.opensaml.ws.soap.util.SOAPConstants;
+import org.opensaml.xml.io.MarshallingException;
 import org.opensaml.xml.io.Unmarshaller;
 import org.opensaml.xml.io.UnmarshallingException;
 import org.opensaml.xml.parse.XMLParserException;
+import org.opensaml.xml.util.XMLHelper;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -37,9 +39,14 @@ public class SOAPTest extends BaseTestCase {
     /** Path, on classpath, to SOAP fault test document. */
     private String soapFault;
     
+    /** Path, on classpath, to SOAP fault test document. */
+    private String soapFaultMarshall;
+    
     private QName expectedFaultCode;
     
     private String expectedFaultString;
+    
+    private String expectedFaultActor;
 
     /** {@inheritDoc} */
     protected void setUp() throws Exception {
@@ -47,9 +54,11 @@ public class SOAPTest extends BaseTestCase {
         
         soapMessage = "/data/org/opensaml/ws/soap/soap11/SOAP.xml";
         soapFault = "/data/org/opensaml/ws/soap/soap11/SOAPFault.xml";
+        soapFaultMarshall = "/data/org/opensaml/ws/soap/soap11/SOAPFaultMarshall.xml";
         
         expectedFaultCode = new QName(SOAPConstants.SOAP11_NS, "Server", SOAPConstants.SOAP11_PREFIX);
         expectedFaultString = "Server Error";
+        expectedFaultActor = "http://ws.example.org/someActor";
     }
     
     /**
@@ -106,15 +115,16 @@ public class SOAPTest extends BaseTestCase {
         assertNotNull("Fault was null", fault);
         
         FaultActor actor = fault.getActor();
-        assertNull("Actor was not null", actor);
+        assertNotNull("FaultActor was null", actor);
+        assertEquals("FaultActor had unexpected value", expectedFaultActor, actor.getValue());
         
         FaultCode code = fault.getCode();
         assertNotNull("FaultCode was null", code);
-        assertEquals("FaultCode had unexpected value", code.getValue(), expectedFaultCode);
+        assertEquals("FaultCode had unexpected value", expectedFaultCode, code.getValue());
         
         FaultString message = fault.getMessage();
         assertNotNull("FaultString was null", message);
-        assertEquals("FaultString had unexpected value", message.getValue(), expectedFaultString);
+        assertEquals("FaultString had unexpected value", expectedFaultString, message.getValue());
         
         Detail detail = fault.getDetail();
         assertNotNull("Detail was null", detail);
@@ -124,5 +134,44 @@ public class SOAPTest extends BaseTestCase {
         envelope.releaseDOM();
         envelope.releaseChildrenDOM(true);
         assertEquals("Marshalled DOM was not the same as control DOM", soapFaultDoc, envelope);
+    }
+    
+    /**
+     * Test constructing and marshalling a SOAP fault message.
+     * 
+     * @throws MarshallingException  if the DOM can not b marshalled
+     * @throws XMLParserException 
+     */
+    public void testSOAPFaultConstructAndMarshall() throws MarshallingException, XMLParserException {
+        Document soapDoc = parserPool.parse(SOAPTest.class.getResourceAsStream(soapFaultMarshall));
+        Element envelopeElem = soapDoc.getDocumentElement();
+        
+        Envelope envelope = (Envelope) buildXMLObject(Envelope.DEFAULT_ELEMENT_NAME);
+        
+        Body body = (Body) buildXMLObject(Body.DEFAULT_ELEMENT_NAME);
+        envelope.setBody(body);
+        
+        Fault fault = (Fault) buildXMLObject(Fault.DEFAULT_ELEMENT_NAME);
+        body.getUnknownXMLObjects().add(fault);
+        
+        FaultCode faultCode = (FaultCode) buildXMLObject(FaultCode.DEFAULT_ELEMENT_NAME);
+        faultCode.setValue(expectedFaultCode);
+        fault.setCode(faultCode);
+        
+        FaultString faultString = (FaultString) buildXMLObject(FaultString.DEFAULT_ELEMENT_NAME);
+        faultString.setValue(expectedFaultString);
+        fault.setMessage(faultString);
+        
+        FaultActor faultActor = (FaultActor) buildXMLObject(FaultActor.DEFAULT_ELEMENT_NAME);
+        faultActor.setValue(expectedFaultActor);
+        fault.setActor(faultActor);
+        
+        Detail detail = (Detail) buildXMLObject(Detail.DEFAULT_ELEMENT_NAME);
+        fault.setDetail(detail);
+        
+        Element marshalledEnvelope = marshallerFactory.getMarshaller(envelope).marshall(envelope);
+        System.out.println(XMLHelper.prettyPrintXML(marshalledEnvelope));
+        assertEquals("Marshalled DOM was not the same as control DOM", soapDoc, envelope);
+        
     }
 }
