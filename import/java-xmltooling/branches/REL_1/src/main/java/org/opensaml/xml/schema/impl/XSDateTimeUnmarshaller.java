1d7cf8c905bf2c5b2b52154f74efb8559dfef71b
putmanb <putmanb@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2010-04-06 00:06:05 +0000

    Explicitly note ISO UTC chronology on parsed DateTime objects.

--- import/java-xmltooling/branches/REL_1/src/main/java/org/opensaml/xml/schema/impl/XSDateTimeUnmarshaller.java
+++ import/java-xmltooling/branches/REL_1/src/main/java/org/opensaml/xml/schema/impl/XSDateTimeUnmarshaller.java
@@ -17,6 +17,7 @@
 package org.opensaml.xml.schema.impl;
 
 import org.joda.time.DateTime;
+import org.joda.time.chrono.ISOChronology;
 import org.opensaml.xml.XMLObject;
 import org.opensaml.xml.io.BaseXMLObjectUnmarshaller;
 import org.opensaml.xml.schema.XSDateTime;
@@ -33,7 +34,7 @@ public class XSDateTimeUnmarshaller extends BaseXMLObjectUnmarshaller{
     protected void processElementContent(XMLObject xmlObject, String elementContent) {
         XSDateTime xsDateTime = (XSDateTime) xmlObject;
         
-        xsDateTime.setValue(new DateTime(elementContent));
+        xsDateTime.setValue(new DateTime(elementContent).withChronology(ISOChronology.getInstanceUTC()));
     }
 
 }
