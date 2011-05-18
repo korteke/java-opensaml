0f316c73c1b5b362839c8b27e8ff850a65145764
putmanb <putmanb@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2011-05-18 15:50:57 +0000

    Rename class for consistency.

--- opensaml-core/src/main/java/org/opensaml/core/XMLObjectProviderInitializer.java
+++ opensaml-core/src/main/java/org/opensaml/core/XMLObjectProviderInitializer.java
@@ -20,7 +20,7 @@ import org.opensaml.xml.AbstractXMLObjectProviderInitializer;
 /**
  * XMLObject provider initializer for module "core".
  */
-public class XMLProviderInitializer extends AbstractXMLObjectProviderInitializer {
+public class XMLObjectProviderInitializer extends AbstractXMLObjectProviderInitializer {
     
     /** Config resources. */
     private static String[] configs = {
