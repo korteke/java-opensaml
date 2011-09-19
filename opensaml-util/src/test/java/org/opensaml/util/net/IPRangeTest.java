39c27b3eeb4680add92548d8198d9fdd414ea73d
iay <iay@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2011-09-19 17:08:58 +0000

    Make IPRange less tolerant of invalid CIDR block notation. As agreed with Chad, only accept 4-component IPv4 addresses and the "preferred" 8-component IPv6 addresses. Included unit tests.

---
+++ opensaml-util/src/test/java/org/opensaml/util/net/IPRangeTest.java
@@ -0,0 +1,50 @@
+
+package org.opensaml.util.net;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class IPRangeTest {
+
+    private void testInvalid(final String address) {
+        try {
+            IPRange.parseCIDRBlock(address);
+            Assert.fail("address should have been invalid: " + address);
+        } catch (IllegalArgumentException e) {
+            // expected behaviour
+            return;
+        }
+    }
+    
+    @Test
+    public void validV4Addresses() {
+        IPRange.parseCIDRBlock("1.2.3.4/32");
+        IPRange.parseCIDRBlock("0.0.0.0/8");
+    }
+    
+    @Test
+    public void validV6Addresses() {
+        IPRange.parseCIDRBlock("0:0:0:0:0:0:0:0/128");
+        IPRange.parseCIDRBlock("1234:5678:90ab:cdef:FfFf:AaAa:BBBB:CCCC/128");
+    }
+    
+    @Test
+    public void invalidJunkAddresses() {
+        testInvalid("/32");
+        testInvalid("f/32");
+    }
+    
+    @Test
+    public void invalidV4Addresses() {
+        testInvalid("1/32");
+        testInvalid("1.2/32");
+        testInvalid("1.2.3/32");
+    }
+    
+    @Test
+    public void invalidV6Addresses() {
+        testInvalid("0:0/128");
+        testInvalid("1:2:3:4:5:6:7/128");
+    }
+
+}
