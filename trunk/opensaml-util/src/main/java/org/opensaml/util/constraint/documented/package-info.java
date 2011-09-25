4e1dc0ada12fb9d190e937984fa6a4ceaef86661
lajoie <lajoie@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2011-09-25 20:07:27 +0000

    Create a set of annotations that are used to document object constraints

---
+++ trunk/opensaml-util/src/main/java/org/opensaml/util/constraint/documented/package-info.java
@@ -0,0 +1,23 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** 
+ * A set of annotations used to document constraints on methods, fields, parameters, etc. Note, these annotations are
+ * for documentation purposes only.  They do not have any effect on the executing code.  Use a JSR-303 implementations
+ * if that's what you're after.
+ */
+package org.opensaml.util.constraint.documented;
