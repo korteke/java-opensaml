82315fe19d1720d79a97b9bb83282c4b27080672
putmanb <putmanb@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2012-01-21 02:08:37 +0000

    Collapse Subcontext and SubcontextContainer into unified Context interface. Implement new AbstractContext base implementation.

--- trunk/opensaml-messaging-api/src/main/java/org/opensaml/messaging/context/AbstractContext.java
+++ trunk/opensaml-messaging-api/src/main/java/org/opensaml/messaging/context/AbstractContext.java
@@ -20,41 +20,145 @@ package org.opensaml.messaging.context;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Iterator;
+import java.util.UUID;
 
 import net.shibboleth.utilities.java.support.collection.ClassIndexedSet;
+import net.shibboleth.utilities.java.support.logic.Assert;
+import net.shibboleth.utilities.java.support.primitive.StringSupport;
 
+import org.joda.time.DateTime;
 import org.opensaml.messaging.MessageRuntimeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 /**
- * Abstract implementation of {@link SubcontextContainer}.
+ * Abstract implementation of {@link Context}.
  */
-public abstract class AbstractSubcontextContainer implements SubcontextContainer {
-    
+public abstract class AbstractContext implements Context {
+
     /** Logger. */
-    private final Logger log = LoggerFactory.getLogger(AbstractSubcontextContainer.class);
+    private final Logger log = LoggerFactory.getLogger(AbstractContext.class);
     
+    /** The owning parent context. */
+    private Context parent;
+
     /** The subcontexts being managed. */
-    private ClassIndexedSet<Subcontext> subcontexts;
+    private ClassIndexedSet<Context> subcontexts;
+    
+    /** The context id. */
+    private String id;
+    
+    /** The context creation time. */
+    private DateTime creationTime;
     
     /** Flag indicating whether subcontexts should, by default, be created if they do not exist. */
     private boolean autoCreateSubcontexts;
     
-    /** Constructor. */
-    public AbstractSubcontextContainer() {
-        subcontexts = new ClassIndexedSet<Subcontext>();
+    /** Constructor. Generates a random context id. */
+    public AbstractContext() {
+        subcontexts = new ClassIndexedSet<Context>();
+        id = UUID.randomUUID().toString();
+        creationTime = new DateTime();
         autoCreateSubcontexts = true;
     }
     
+    /**
+     * Constructor.
+     * 
+     * @param contextId ID for this context, not null nor empty
+     */
+    public AbstractContext(final String contextId) {
+        this();
+        setId(contextId);
+    }
+    
+    /**
+     * Constructor. Adds this context as a child of the given parent.
+     * Generates a random context id.
+     * 
+     * @param newParent the owning parent context.
+     */
+    public AbstractContext(final Context newParent) {
+        this();
+        setParent(newParent);
+    }
+    
+    /**
+     * Constructor.
+     * 
+     * @param contextId ID for this context, not null nor empty
+     * @param newParent the owning parent context.
+     */
+    public AbstractContext(final String contextId, final Context newParent) {
+        this();
+        setId(contextId);
+        setParent(newParent);
+    }
+    
     /** {@inheritDoc} */
-    public <T extends Subcontext> T getSubcontext(Class<T> clazz) {
+    public String getId() {
+        return id;
+    }
+    
+    /**
+     * Set the context id. 
+     * 
+     * @param newId the new context id
+     */
+    protected void setId(final String newId) {
+        id = Assert.isNotNull(StringSupport.trimOrNull(newId), "Context ID can not be null or empty");
+    }
+    
+    /** {@inheritDoc} */
+    public DateTime getCreationTime() {
+        return creationTime;
+    }
+    
+    /** {@inheritDoc} */
+    public Context getParent() {
+        return parent;
+    }
+    
+    /**
+     * Set the context parent. 
+     * 
+     * @param newParent the new context parent
+     */
+    protected void setParent(final Context newParent) {
+        Context oldParent = parent;
+        parent = newParent;
+        
+        if (parent != null) {
+            log.trace("Setting context with type '{}' with id '{}' as parent of context with type '{}' with id '{}'", 
+                    new String[]{parent.getClass().getName(), parent.getId(), this.getClass().getName(), this.getId()});
+            Context currentChild = parent.getSubcontext(this.getClass());
+            if (currentChild == null) {
+                log.trace("Adding context to parent");
+                parent.addSubcontext(this);
+            } else if (currentChild == this) {
+                log.trace("Context was already in parent's subcontext collection");
+            } else {
+                // TODO: not sure what to do here, perhaps either throw exception or else do a force add.
+               log.warn("A different instance of type {} was a child of parent context, could not add this instance");
+            }
+        } else {
+           log.trace("New parent context was null for context '{}' with id '{}'", this.getClass().getName(), this.getId());
+        }
+        
+        if (oldParent != null) {
+            log.trace("Removing context from old parent");
+            oldParent.removeSubcontext(this);
+        }
+    }
+    
+    /** {@inheritDoc} */
+    public <T extends Context> T getSubcontext(Class<T> clazz) {
         return getSubcontext(clazz, isAutoCreateSubcontexts());
     }
     
     /** {@inheritDoc} */
-    public <T extends Subcontext> T getSubcontext(Class<T> clazz, boolean autocreate) {
+    public <T extends Context> T getSubcontext(Class<T> clazz, boolean autocreate) {
         log.trace("Request for subcontext of type: {}", clazz.getName());
         T subcontext = subcontexts.get(clazz);
         if (subcontext != null) {
@@ -74,35 +178,35 @@ public abstract class AbstractSubcontextContainer implements SubcontextContainer
     }
     
     /** {@inheritDoc} */
-    public void addSubcontext(Subcontext subContext) {
+    public void addSubcontext(Context subContext) {
         addSubcontext(subContext, false);
     }
     
     /** {@inheritDoc} */
-    public void addSubcontext(Subcontext subContext, boolean replace) {
+    public void addSubcontext(Context subContext, boolean replace) {
         subcontexts.add(subContext, replace);
     }
     
     /** {@inheritDoc} */
-    public void removeSubcontext(Subcontext subcontext) {
+    public void removeSubcontext(Context subcontext) {
         subcontexts.remove(subcontext);
     }
     
     /** {@inheritDoc} */
-    public <T extends Subcontext>void removeSubcontext(Class<T> clazz) {
-        Subcontext subcontext = getSubcontext(clazz, false);
+    public <T extends Context>void removeSubcontext(Class<T> clazz) {
+        Context subcontext = getSubcontext(clazz, false);
         if (subcontext != null) {
             removeSubcontext(subcontext);
         }
     }
     
     /** {@inheritDoc} */
-    public Iterator<Subcontext>  iterator() {
+    public Iterator<Context>  iterator() {
         return subcontexts.iterator();
     }
     
     /** {@inheritDoc} */
-    public <T extends Subcontext> boolean containsSubcontext(Class<T> clazz) {
+    public <T extends Context> boolean containsSubcontext(Class<T> clazz) {
         return subcontexts.contains(clazz);
     }
     
@@ -128,10 +232,10 @@ public abstract class AbstractSubcontextContainer implements SubcontextContainer
      * @param clazz the class of the subcontext instance to create
      * @return the new subcontext instance
      */
-    protected <T extends Subcontext> T createSubcontext(Class<T> clazz) {
+    protected <T extends Context> T createSubcontext(Class<T> clazz) {
         Constructor<T> constructor;
         try {
-            constructor = clazz.getConstructor(new Class[] {SubcontextContainer.class});
+            constructor = clazz.getConstructor(new Class[] {Context.class});
             return constructor.newInstance(new Object[] { this });
         } catch (SecurityException e) {
             log.error("Security error on creating subcontext", e);
--- trunk/opensaml-messaging-api/src/main/java/org/opensaml/messaging/context/Context.java
+++ trunk/opensaml-messaging-api/src/main/java/org/opensaml/messaging/context/Context.java
@@ -17,14 +17,31 @@
 
 package org.opensaml.messaging.context;
 
+import java.util.Iterator;
+
 import net.shibboleth.utilities.java.support.component.IdentifiableComponent;
 
 import org.joda.time.DateTime;
 
 /**
  * Interface for a component which represents the context used to store state used for purposes related to messaging.
+ * 
+ * <p>
+ * A Context may also function as a container of subcontexts.
+ * Access to subcontexts is class-based.  The parent context may hold only
+ * one instance of a given class at a given time.  This class-based indexing approach
+ * is used to enforce type-safety over the subcontext instances returned from the parent context,
+ * and avoids the need for casting.
+ * </p>
+ * 
+ * <p>
+ * When a subcontext is requested and it does not exist in the parent context, it may optionally be
+ * auto-created.  In order to auto-created in this manner, the subcontext type
+ * <strong>MUST</strong> have a single-arg constructor which takes a <code>Context</code>.
+ * If the requested subcontext does not conform to this convention, auto-creation will fail.
+ * </p>
  */
-public interface Context extends IdentifiableComponent {
+public interface Context extends IdentifiableComponent, Iterable<Context> {
 
     /**
      * Get the timestamp of the creation of the context.
@@ -32,5 +49,95 @@ public interface Context extends IdentifiableComponent {
      * @return the creation timestamp
      */
     public DateTime getCreationTime();
+    
+    /**
+     * Get the parent context.
+     * 
+     * @return the parent context or null 
+     */
+    public Context getParent();
+    
+    /**
+     * Get a subcontext from the container.
+     * 
+     * @param <T> the type of subcontext being operated on
+     * @param clazz the class type to obtain
+     * @return the held instance of the class, or null
+     */
+    public <T extends Context> T getSubcontext(Class<T> clazz);
+    
+    /**
+     * Get a subcontext from the container.
+     * 
+     * @param <T> the type of subcontext being operated on
+     * @param clazz the class type to obtain
+     * @param autocreate flag indicating whether the subcontext instance should be auto-created
+     * @return the held instance of the class, or null
+     */
+    public <T extends Context> T getSubcontext(Class<T> clazz, boolean autocreate);
+    
+    /**
+     * Add a subcontext to the container.
+     * 
+     * @param subContext the subcontext to add
+     */
+    public void addSubcontext(Context subContext);
+    
+    /**
+     * Add a subcontext to the container.
+     * 
+     * @param subContext the subcontext to add
+     * @param replace flag indicating whether to replace the existing instance of the subcontext if present
+     * 
+     */
+    public void addSubcontext(Context subContext, boolean replace);
+    
+    /**
+     * Remove a subcontext from the container.
+     * 
+     * @param <T> the type of subcontext being operated on
+     * @param subcontext the subcontext to remove
+     */
+    public <T extends Context> void removeSubcontext(Context subcontext);
+    
+    /**
+     * Remove the subcontext from the container which corresponds to the supplied class.
+     * 
+     * @param <T> the type of subcontext being operated on
+     * @param clazz the subcontext class to remove
+     */
+    public <T extends Context> void removeSubcontext(Class<T> clazz);
+    
+    /**
+     * Return whether the container currently contains an instance of
+     * the specified subcontext class.
+     * 
+     * @param <T> the type of subcontext being operated on
+     * @param clazz the class to check
+     * @return true if the container contains an instance of the class, false otherwise
+     */
+    public <T extends Context> boolean containsSubcontext(Class<T> clazz);
+    
+    /**
+     * Clear the subcontext container.
+     */
+    public void clearSubcontexts();
+    
+    /**
+     * Get whether the container auto-creates subcontexts by default.
+     * 
+     * @return true if the container auto-creates subcontexts, false otherwise
+     */
+    public boolean isAutoCreateSubcontexts();
+    
+    /**
+     * Set whether the container auto-creates subcontexts by default.
+     * 
+     * @param autoCreate whether the container should auto-create subcontexts
+     */
+    public void setAutoCreateSubcontexts(boolean autoCreate);
+    
+    /** {@inheritDoc} */
+    public Iterator<Context>  iterator();
 
 }
