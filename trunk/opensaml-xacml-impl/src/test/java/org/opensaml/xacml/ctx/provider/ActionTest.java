13dafebade3ad0d6094aa5740851480cdc1aa306
rdw <rdw@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2012-06-30 14:31:38 +0000

    ActionType is a provider, not a ctx

---
+++ trunk/opensaml-xacml-impl/src/test/java/org/opensaml/xacml/ctx/provider/ActionTest.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.xacml.ctx.provider;
+
+import org.opensaml.core.xml.XMLObjectProviderBaseTestCase;
+import org.opensaml.xacml.policy.ActionMatchType;
+import org.opensaml.xacml.policy.AttributeDesignatorType;
+import org.opensaml.xacml.policy.AttributeSelectorType;
+import org.opensaml.xacml.policy.AttributeValueType;
+import org.opensaml.xacml.policy.impl.ActionMatchTypeImplBuilder;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Test case for creating, marshalling, and unmarshalling
+ * {@link org.opensaml.saml.saml2.metadata.impl.AttributeProfileImpl}.
+ */
+public class ActionTest extends XMLObjectProviderBaseTestCase {
+    
+    /** Expected ProfileURI */
+    private String expectedMatchId;
+    
+    /**
+     * Constructor
+     */
+    public ActionTest(){
+        singleElementFile = "/data/org/opensaml/xacml/policy/impl/Action.xml";
+        childElementsFile  = "/data/org/opensaml/xacml/policy/impl/ActionChildElements.xml";
+    }
+    
+    /** {@inheritDoc} */
+    @BeforeMethod
+    protected void setUp() throws Exception {
+        expectedMatchId = "http://example.org";
+    }
+
+    /** {@inheritDoc} */
+    @Test
+    public void testSingleElementUnmarshall(){
+        ActionMatchType actionMatch = (ActionMatchType) unmarshallElement(singleElementFile);
+        
+        Assert.assertEquals(actionMatch.getMatchId(), expectedMatchId, "MatchId URI has a value of " + actionMatch.getMatchId() + ", expected a value of " + expectedMatchId);
+    }
+
+    /** {@inheritDoc} */
+    @Test
+    public void testSingleElementMarshall(){
+        ActionMatchType actionMatch = (new ActionMatchTypeImplBuilder()).buildObject();
+        
+        actionMatch.setMatchId(expectedMatchId);
+        
+        assertXMLEquals(expectedDOM, actionMatch);
+    }
+    
+    /** {@inheritDoc} */
+    @Test
+    public void testChildElementsUnmarshall(){
+        ActionMatchType actionMatch = (ActionMatchType) unmarshallElement(childElementsFile);
+        
+        Assert.assertNotNull(actionMatch.getActionAttributeDesignator());
+        Assert.assertNotNull(actionMatch.getAttributeSelector());
+        Assert.assertNotNull(actionMatch.getAttributeValue());
+        
+        Assert.assertEquals(actionMatch.getMatchId(), expectedMatchId, "MatchId URI has a value of " + actionMatch.getMatchId() + ", expected a value of " + expectedMatchId);
+    }
+
+    /** {@inheritDoc} */
+    @Test
+    public void testChildElementsMarshall(){
+        ActionMatchType actionMatch = (new ActionMatchTypeImplBuilder()).buildObject();
+        actionMatch.setMatchId(expectedMatchId);
+        
+        AttributeDesignatorType attributeDesignatorType = (AttributeDesignatorType) buildXMLObject(AttributeDesignatorType.ACTION_ATTRIBUTE_DESIGNATOR_ELEMENT_NAME);
+        attributeDesignatorType.setMustBePresent(false);
+        actionMatch.setActionAttributeDesignator(attributeDesignatorType);
+
+        actionMatch.setAttributeSelector((AttributeSelectorType) buildXMLObject(AttributeSelectorType.DEFAULT_ELEMENT_NAME));
+        actionMatch.setAttributeValue((AttributeValueType) buildXMLObject(AttributeValueType.DEFAULT_ELEMENT_NAME));
+
+        assertXMLEquals(expectedChildElementsDOM, actionMatch);
+}
+}
