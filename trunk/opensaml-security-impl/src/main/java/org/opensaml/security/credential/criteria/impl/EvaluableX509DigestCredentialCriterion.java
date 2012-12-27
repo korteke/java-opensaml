ba9e66f51280a00aeeba48eeb2c320149d444b1f
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2012-12-27 19:46:04 +0000

    Add X509 digest evaluator and unit test.

---
+++ trunk/opensaml-security-impl/src/main/java/org/opensaml/security/credential/criteria/impl/EvaluableX509DigestCredentialCriterion.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.security.credential.criteria.impl;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.bouncycastle.util.Arrays;
+import org.opensaml.security.credential.Credential;
+import org.opensaml.security.x509.X509Credential;
+import org.opensaml.security.x509.X509DigestCriterion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.shibboleth.utilities.java.support.logic.Constraint;
+import net.shibboleth.utilities.java.support.primitive.StringSupport;
+
+
+/**
+ * An implementation of {@link Criterion} which specifies criteria based on
+ * the digest of an X.509 certificate.
+ */
+public final class EvaluableX509DigestCredentialCriterion implements EvaluableCredentialCriterion {
+
+    /** Logger. */
+    private final Logger log = LoggerFactory.getLogger(EvaluableX509DigestCredentialCriterion.class);
+    
+    /** Digest algorithm. */
+    private final String algorithm;
+    
+    /** X.509 certificate digest. */
+    private final byte[] x509digest;
+
+    /**
+     * Constructor.
+     * 
+     * @param criteria the criteria which is the basis for evaluation
+     */
+    public EvaluableX509DigestCredentialCriterion(@Nonnull final X509DigestCriterion criteria) {
+        algorithm = Constraint.isNotNull(criteria, "Criterion instance cannot be null").getAlgorithm();
+        x509digest = criteria.getDigest();
+    }
+    
+    /**
+     * Constructor.
+     *
+     * @param alg algorithm of digest computation
+     * @param digest certificate digest
+     */
+    public EvaluableX509DigestCredentialCriterion(@Nonnull final String alg, @Nonnull final byte[] digest) {
+        x509digest = Constraint.isNotEmpty(digest, "X.509 digest cannot be null or empty");
+        String trimmed = StringSupport.trimOrNull(alg);
+        Constraint.isNotNull(trimmed, "Certificate digest algorithm cannot be null or empty");
+        algorithm = trimmed;
+    }
+
+    /** {@inheritDoc} */
+    @Nullable public Boolean evaluate(@Nullable final Credential target) {
+        if (target == null) {
+            log.error("Credential target was null");
+            return null;
+        } else if (!(target instanceof X509Credential)) {
+            log.info("Credential is not an X509Credential, does not satisfy X.509 digest criteria");
+            return Boolean.FALSE;
+        }
+
+        X509Certificate entityCert = ((X509Credential) target).getEntityCertificate();
+        if (entityCert == null) {
+            log.info("X509Credential did not contain an entity certificate, does not satisfy criteria");
+            return Boolean.FALSE;
+        }
+        
+        try {
+            MessageDigest hasher = MessageDigest.getInstance(algorithm);
+            byte[] hashed = hasher.digest(entityCert.getEncoded());
+            return Arrays.areEqual(hashed, x509digest);
+        } catch (CertificateEncodingException e) {
+            log.error("Unable to encode certificate for digest operation", e);
+        } catch (NoSuchAlgorithmException e) {
+            log.error("Unable to obtain a digest implementation for algorithm {" + algorithm + "}" , e);
+        }
+        
+        return null;
+    }
+    
+}
---
+++ trunk/opensaml-security-impl/src/test/java/org/opensaml/security/credential/criteria/impl/EvaluableX509DigestCredentialCriterionTest.java
@@ -0,0 +1,124 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.security.credential.criteria.impl;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.Assert;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.cert.X509Certificate;
+
+import net.shibboleth.utilities.java.support.codec.Base64Support;
+
+import org.opensaml.security.credential.BasicCredential;
+import org.opensaml.security.credential.criteria.impl.EvaluableCredentialCriteriaRegistry;
+import org.opensaml.security.credential.criteria.impl.EvaluableCredentialCriterion;
+import org.opensaml.security.crypto.KeySupport;
+import org.opensaml.security.x509.BasicX509Credential;
+import org.opensaml.security.x509.X509DigestCriterion;
+import org.opensaml.security.x509.X509Support;
+
+/**
+ *
+ */
+public class EvaluableX509DigestCredentialCriterionTest {
+    
+    private BasicX509Credential credential;
+    private String digestAlgorithm = "SHA256";
+    private String entityCertDigestBase64 = "z+OxxIy+EZxLN6PbDEXPmOutQhaYbcJYKDrIyFSuE0I=";
+    private byte[] entityCertDigest;
+    
+    private X509DigestCriterion criteria;
+    
+    
+    private X509Certificate entityCert;
+    private String entityCertBase64 = 
+        "MIIDzjCCAragAwIBAgIBMTANBgkqhkiG9w0BAQUFADAtMRIwEAYDVQQKEwlJbnRl" +
+        "cm5ldDIxFzAVBgNVBAMTDmNhLmV4YW1wbGUub3JnMB4XDTA3MDUyMTE4MjM0MFoX" +
+        "DTE3MDUxODE4MjM0MFowMTESMBAGA1UEChMJSW50ZXJuZXQyMRswGQYDVQQDExJm" +
+        "b29iYXIuZXhhbXBsZS5vcmcwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIB" +
+        "AQDNWnkFmhy1vYa6gN/xBRKkZxFy3sUq2V0LsYb6Q3pe9Qlb6+BzaM5DrN8uIqqr" +
+        "oBE3Wp0LtrgKuQTpDpNFBdS2p5afiUtOYLWBDtizTOzs3Z36MGMjIPUYQ4s03IP3" +
+        "yPh2ud6EKpDPiYqzNbkRaiIwmYSit5r+RMYvd6fuKvTOn6h7PZI5AD7Rda7VWh5O" +
+        "VSoZXlRx3qxFho+mZhW0q4fUfTi5lWwf4EhkfBlzgw/k5gf4cOi6rrGpRS1zxmbt" +
+        "X1RAg+I20z6d04g0N2WsK5stszgYKoIROJCiXwjraa8/SoFcILolWQpttVHBIUYl" +
+        "yDlm8mIFleZf4ReFpfm+nUYxAgMBAAGjgfQwgfEwCQYDVR0TBAIwADAsBglghkgB" +
+        "hvhCAQ0EHxYdT3BlblNTTCBHZW5lcmF0ZWQgQ2VydGlmaWNhdGUwHQYDVR0OBBYE" +
+        "FDgRgTkjaKoK6DoZfUZ4g9LDJUWuMFUGA1UdIwROMEyAFNXuZVPeUdqHrULqQW7y" +
+        "r9buRpQLoTGkLzAtMRIwEAYDVQQKEwlJbnRlcm5ldDIxFzAVBgNVBAMTDmNhLmV4" +
+        "YW1wbGUub3JnggEBMEAGA1UdEQQ5MDeCEmFzaW1vdi5leGFtcGxlLm9yZ4YbaHR0" +
+        "cDovL2hlaW5sZWluLmV4YW1wbGUub3JnhwQKAQIDMA0GCSqGSIb3DQEBBQUAA4IB" +
+        "AQBLiDMyQ60ldIytVO1GCpp1S1sKJyTF56GVxHh/82hiRFbyPu+2eSl7UcJfH4ZN" +
+        "bAfHL1vDKTRJ9zoD8WRzpOCUtT0IPIA/Ex+8lFzZmujO10j3TMpp8Ii6+auYwi/T" +
+        "osrfw1YCxF+GI5KO49CfDRr6yxUbMhbTN+ssK4UzFf36UbkeJ3EfDwB0WU70jnlk" +
+        "yO8f97X6mLd5QvRcwlkDMftP4+MB+inTlxDZ/w8NLXQoDW6p/8r91bupXe0xwuyE" +
+        "vow2xjxlzVcux2BZsUZYjBa07ZmNNBtF7WaQqH7l2OBCAdnBhvme5i/e0LK3Ivys" +
+        "+hcVyvCXs5XtFTFWDAVYvzQ6";
+        
+    
+    public EvaluableX509DigestCredentialCriterionTest() {
+        
+    }
+
+    /** {@inheritDoc} */
+    @BeforeMethod
+    protected void setUp() throws Exception {
+        entityCert = X509Support.decodeCertificate(entityCertBase64);
+        entityCertDigest = Base64Support.decode(entityCertDigestBase64);
+        
+        credential = new BasicX509Credential(entityCert);
+        
+        criteria = new X509DigestCriterion(digestAlgorithm, entityCertDigest);
+    }
+    
+    @Test
+    public void testSatifsy() {
+        EvaluableX509DigestCredentialCriterion evalCrit = new EvaluableX509DigestCredentialCriterion(criteria);
+        Assert.assertTrue(evalCrit.evaluate(credential), "Credential should have matched the evaluable criteria");
+    }
+
+    @Test
+    public void testNotSatisfy() {
+        criteria.setAlgorithm("SHA1");
+        EvaluableX509DigestCredentialCriterion evalCrit = new EvaluableX509DigestCredentialCriterion(criteria);
+        Assert.assertFalse(evalCrit.evaluate(credential), "Credential should NOT have matched the evaluable criteria");
+    }
+    
+    @Test
+    public void testNotSatisfyWrongCredType() throws NoSuchAlgorithmException, NoSuchProviderException {
+        BasicCredential basicCred = new BasicCredential(KeySupport.generateKey("AES", 128, null));
+        EvaluableX509DigestCredentialCriterion evalCrit = new EvaluableX509DigestCredentialCriterion(criteria);
+        Assert.assertFalse(evalCrit.evaluate(basicCred), "Credential should NOT have matched the evaluable criteria");
+    }
+    
+    @Test
+    public void testCanNotEvaluate() {
+        criteria.setAlgorithm("SHA0");
+        EvaluableX509DigestCredentialCriterion evalCrit = new EvaluableX509DigestCredentialCriterion(criteria);
+        Assert.assertNull(evalCrit.evaluate(credential), "Credential should have been unevaluable against the criteria");
+    }
+    
+    @Test
+    public void testRegistry() throws Exception {
+        EvaluableCredentialCriterion evalCrit = EvaluableCredentialCriteriaRegistry.getEvaluator(criteria);
+        Assert.assertNotNull(evalCrit, "Evaluable criteria was unavailable from the registry");
+        Assert.assertTrue(evalCrit.evaluate(credential), "Credential should have matched the evaluable criteria");
+    }
+}
