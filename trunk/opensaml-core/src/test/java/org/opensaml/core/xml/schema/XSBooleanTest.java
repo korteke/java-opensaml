4ed60a05cec9388f43c4296ff0d97a14a95d4e0a
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2013-01-30 05:30:33 +0000

    Port java-xmltooling r747

---
+++ trunk/opensaml-core/src/test/java/org/opensaml/core/xml/schema/XSBooleanTest.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.core.xml.schema;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.Assert;
+import javax.xml.namespace.QName;
+
+import net.shibboleth.utilities.java.support.xml.XMLParserException;
+
+import org.opensaml.core.xml.XMLObjectBaseTestCase;
+import org.opensaml.core.xml.io.Marshaller;
+import org.opensaml.core.xml.io.MarshallingException;
+import org.opensaml.core.xml.io.Unmarshaller;
+import org.opensaml.core.xml.io.UnmarshallingException;
+import org.opensaml.core.xml.schema.XSString;
+import org.opensaml.core.xml.schema.impl.XSBooleanBuilder;
+import org.w3c.dom.Document;
+
+/**
+ * Unit test for {@link XSBoolean}
+ */
+public class XSBooleanTest extends XMLObjectBaseTestCase {
+    
+    private String testDocumentLocation;
+    private QName expectedXMLObjectQName;
+    private String expectedValue;
+    
+    /** {@inheritDoc} */
+    @BeforeMethod
+    protected void setUp() throws Exception{
+        testDocumentLocation = "/data/org/opensaml/core/xml/schema/xsBoolean.xml";
+        expectedXMLObjectQName = new QName("urn:example.org:foo", "bar", "foo");
+        expectedValue = "true";
+    }
+
+    /**
+     * Tests Marshalling a boolean type.
+     * @throws MarshallingException 
+     * @throws XMLParserException 
+     */
+    @Test
+    public void testMarshall() throws MarshallingException, XMLParserException{
+        XSBooleanBuilder xsbBuilder = (XSBooleanBuilder) builderFactory.getBuilder(XSBoolean.TYPE_NAME);
+        XSBoolean xsBoolean = xsbBuilder.buildObject(expectedXMLObjectQName, XSBoolean.TYPE_NAME);
+        xsBoolean.setValue(XSBooleanValue.valueOf(expectedValue));
+        
+        Marshaller marshaller = marshallerFactory.getMarshaller(xsBoolean);
+        marshaller.marshall(xsBoolean);
+        
+        Document document = parserPool.parse(XSBooleanTest.class.getResourceAsStream(testDocumentLocation));
+        assertXMLEquals("Marshalled XSBoolean does not match example document", document, xsBoolean);
+    }
+    
+    /**
+     * Tests Marshalling a string type.
+     * 
+     * @throws XMLParserException 
+     * @throws UnmarshallingException 
+     */
+    @Test
+    public void testUnmarshall() throws XMLParserException, UnmarshallingException{
+        Document document = parserPool.parse(XSBooleanTest.class.getResourceAsStream(testDocumentLocation));
+
+        Unmarshaller unmarshaller = unmarshallerFactory.getUnmarshaller(document.getDocumentElement());
+        XSBoolean xsBoolean = (XSBoolean) unmarshaller.unmarshall(document.getDocumentElement());
+        
+        Assert.assertEquals(xsBoolean.getElementQName(), expectedXMLObjectQName, "Unexpected XSBoolean QName");
+        Assert.assertEquals(xsBoolean.getSchemaType(), XSBoolean.TYPE_NAME, "Unexpected XSBoolean schema type");
+        Assert.assertEquals(expectedValue, xsBoolean.getValue().toString(), "Unexpected value of XSBoolean");
+    }
+}
