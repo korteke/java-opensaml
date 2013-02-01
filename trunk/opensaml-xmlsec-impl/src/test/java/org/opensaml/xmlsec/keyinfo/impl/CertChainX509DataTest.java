43db068f8427e32aa5c39dc2d30d0f153b16b657
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2013-02-01 05:03:16 +0000

    Port java-xmltooling r781

--- trunk/opensaml-xmlsec-impl/src/test/java/org/opensaml/xmlsec/keyinfo/impl/CertChainX509DataTest.java
+++ trunk/opensaml-xmlsec-impl/src/test/java/org/opensaml/xmlsec/keyinfo/impl/CertChainX509DataTest.java
@@ -242,7 +242,7 @@ public class CertChainX509DataTest extends XMLObjectBaseTestCase {
     }
     
     /**
-     * Test resolution with multiple certs, end-entity cert identified by X509SubjectName.
+     * Test resolution with multiple certs, end-entity cert identified by X509SKI.
      * 
      * @throws SecurityException on error resolving credentials
      * @throws ResolverException on error resolving credentials
