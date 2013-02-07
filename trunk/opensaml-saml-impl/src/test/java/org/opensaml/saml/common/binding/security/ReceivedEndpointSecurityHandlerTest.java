fdcaf61da6d6696be9bee754aaa011369c74a743
putmanb <putmanb@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2013-02-07 01:22:26 +0000

    Unit test for received endpoint check handler.

---
+++ trunk/opensaml-saml-impl/src/test/java/org/opensaml/saml/common/binding/security/ReceivedEndpointSecurityHandlerTest.java
@@ -0,0 +1,152 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.saml.common.binding.security;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.opensaml.core.xml.XMLObjectBaseTestCase;
+import org.opensaml.messaging.MessageException;
+import org.opensaml.messaging.context.MessageContext;
+import org.opensaml.messaging.decoder.servlet.HttpServletRequestContext;
+import org.opensaml.messaging.handler.MessageHandlerException;
+import org.opensaml.saml.common.SAMLObject;
+import org.opensaml.saml.common.binding.SAMLBindingSupport;
+import org.opensaml.saml.common.messaging.context.SamlBindingContext;
+import org.opensaml.saml.common.xml.SAMLConstants;
+import org.opensaml.saml.saml2.core.AuthnRequest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Test the received endpoint check message handler.
+ */
+public class ReceivedEndpointSecurityHandlerTest extends XMLObjectBaseTestCase {
+    
+    private MessageContext<SAMLObject> messageContext;
+    
+    private SamlBindingContext samlBindingContext;
+    
+    private MockHttpServletRequest httpRequest;
+    
+    private String intendedDestinationUri;
+    
+    private ReceivedEndpointSecurityHandler handler;
+    
+    @BeforeMethod
+    public void setUp() throws MessageException {
+        messageContext = new MessageContext<SAMLObject>();
+        messageContext.setMessage((SAMLObject) unmarshallElement("/data/org/opensaml/saml/saml2/binding/AuthnRequest.xml"));
+        
+        httpRequest = new MockHttpServletRequest();
+        messageContext.getSubcontext(HttpServletRequestContext.class, true).setHttpServletRequest(httpRequest);
+        
+        samlBindingContext = messageContext.getSubcontext(SamlBindingContext.class, true);
+        samlBindingContext.setBindingUri(SAMLConstants.SAML2_REDIRECT_BINDING_URI);
+        samlBindingContext.setHasBindingSignature(false);
+        samlBindingContext.setIntendedDestinationEndpointUriRequired(false);
+        
+        intendedDestinationUri = SAMLBindingSupport.getIntendedDestinationEndpointUri(messageContext);
+        
+        handler = new ReceivedEndpointSecurityHandler();
+    }
+    
+    @Test
+    public void testEndpointGood() throws MessageHandlerException {
+        String deliveredEndpointURL = intendedDestinationUri;
+        
+        populateRequestURL(httpRequest, deliveredEndpointURL);
+        
+        handler.invoke(messageContext);
+    }
+    
+    @Test(expectedExceptions=MessageHandlerException.class)
+    public void testEndpointInvalidPath() throws MessageHandlerException {
+        String deliveredEndpointURL = intendedDestinationUri + "/some/other/endpointURI";
+        
+        
+        populateRequestURL(httpRequest, deliveredEndpointURL);
+        
+        handler.invoke(messageContext);
+    }
+    
+    @Test(expectedExceptions=MessageHandlerException.class)
+    public void testEndpointInvalidHost() throws MessageHandlerException {
+        String deliveredEndpointURL = "https://bogusidp.example.com/idp/sso";
+        
+        populateRequestURL(httpRequest, deliveredEndpointURL);
+        
+        handler.invoke(messageContext);
+    }
+    
+    @Test
+    public void testEndpointMissingDestinationNotRequired() throws MessageHandlerException {
+        AuthnRequest authnRequest = (AuthnRequest) messageContext.getMessage();
+        authnRequest.setDestination(null);
+        
+        samlBindingContext.setIntendedDestinationEndpointUriRequired(false);
+        
+        String deliveredEndpointURL = intendedDestinationUri;
+        
+        populateRequestURL(httpRequest, deliveredEndpointURL);
+        
+        handler.invoke(messageContext);
+    }
+    
+    @Test(expectedExceptions=MessageHandlerException.class)
+    public void testEndpointMissingDestinationRequired() throws MessageHandlerException {
+        AuthnRequest authnRequest = (AuthnRequest) messageContext.getMessage();
+        authnRequest.setDestination(null);
+        
+        samlBindingContext.setIntendedDestinationEndpointUriRequired(true);
+        
+        String deliveredEndpointURL = intendedDestinationUri;
+        
+        populateRequestURL(httpRequest, deliveredEndpointURL);
+        
+        handler.invoke(messageContext);
+    }
+    
+    
+    // Helpers
+    
+    private void populateRequestURL(MockHttpServletRequest request, String requestURL) {
+        URL url = null;
+        try {
+            url = new URL(requestURL);
+        } catch (MalformedURLException e) {
+            Assert.fail("Malformed URL: " + e.getMessage());
+        }
+        request.setScheme(url.getProtocol());
+        request.setServerName(url.getHost());
+        if (url.getPort() != -1) {
+            request.setServerPort(url.getPort());
+        } else {
+            if ("https".equalsIgnoreCase(url.getProtocol())) {
+                request.setServerPort(443);
+            } else if ("http".equalsIgnoreCase(url.getProtocol())) {
+                request.setServerPort(80);
+            }
+        }
+        request.setRequestURI(url.getPath());
+        request.setQueryString(url.getQuery());
+    }
+
+}
