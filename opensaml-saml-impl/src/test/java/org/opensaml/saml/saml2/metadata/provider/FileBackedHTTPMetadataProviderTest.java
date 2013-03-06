13f48636d47a606b2870abee9a20404e26157948
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2013-03-06 21:09:10 +0000

    Remove extra import.

--- opensaml-saml-impl/src/test/java/org/opensaml/saml/saml2/metadata/provider/FileBackedHTTPMetadataProviderTest.java
+++ opensaml-saml-impl/src/test/java/org/opensaml/saml/saml2/metadata/provider/FileBackedHTTPMetadataProviderTest.java
@@ -21,7 +21,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.Assert;
-import org.testng.Assert;
 import java.io.File;
 
 import org.opensaml.core.xml.XMLObjectBaseTestCase;
