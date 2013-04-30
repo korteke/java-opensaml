4b4fae7e004eb2b9466c2816ac3f8d65a903f5af
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2013-04-30 17:27:42 +0000

    Initial unit testing of storage, some bug fixes.

---
+++ opensaml-util/src/test/java/org/opensaml/util/storage/StorageServiceTest.java
@@ -0,0 +1,131 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.util.storage;
+
+import java.io.IOException;
+import java.security.SecureRandom;
+
+import javax.annotation.Nonnull;
+
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Test of {@link StorageService} implementations.
+ */
+public abstract class StorageServiceTest {
+    
+    private SecureRandom random;
+    
+    protected StorageService shared;
+
+    /**
+     * Returns a fresh service instance to test.
+     * 
+     * @return  a new instance
+     */
+    @Nonnull protected abstract StorageService getStorageService();
+
+    @BeforeClass
+    protected void setUp() throws ComponentInitializationException {
+        random = new SecureRandom();
+        shared = getStorageService();
+        shared.initialize();
+    }
+    
+    @AfterClass
+    protected void tearDown() {
+        shared.destroy();
+    }
+
+    @Test(threadPoolSize = 10, invocationCount = 10,  timeOut = 10000)
+    public void strings() throws IOException {
+        String context = Long.toString(random.nextLong());
+        
+        for (int i = 1; i <= 100; i++) {
+            shared.createString(context, Integer.toString(i), Integer.toString(i + 1), System.currentTimeMillis() / 1000 + 300);
+        }
+        
+        for (int i = 1; i <= 100; i++) {
+            StorageRecord rec = shared.readString(context, Integer.toString(i));
+            Assert.assertNotNull(rec);
+            Assert.assertEquals(rec.getValue(), Integer.toString(i + 1));
+        }
+
+        for (int i = 1; i <= 100; i++) {
+            shared.updateString(context, Integer.toString(i), Integer.toString(i + 2));
+        }
+
+        for (int i = 1; i <= 100; i++) {
+            StorageRecord rec = shared.readString(context, Integer.toString(i));
+            Assert.assertNotNull(rec);
+            Assert.assertEquals(rec.getValue(), Integer.toString(i + 2));
+        }
+
+        for (int i = 1; i <= 100; i++) {
+            boolean result = shared.createString(context, Integer.toString(i), Integer.toString(i + 1));
+            Assert.assertFalse(result, "createString should have failed");
+        }        
+        
+        for (int i = 1; i <= 100; i++) {
+            shared.deleteString(context, Integer.toString(i));
+            StorageRecord rec = shared.readString(context, Integer.toString(i));
+            Assert.assertNull(rec);
+        }
+    }
+
+    @Test(threadPoolSize = 10, invocationCount = 10,  timeOut = 10000)
+    public void texts() throws IOException {
+        String context = Long.toString(random.nextLong());
+        
+        for (int i = 1; i <= 100; i++) {
+            shared.createText(context, Integer.toString(i), Integer.toString(i + 1), System.currentTimeMillis() / 1000 + 300);
+        }
+        
+        for (int i = 1; i <= 100; i++) {
+            StorageRecord rec = shared.readText(context, Integer.toString(i));
+            Assert.assertNotNull(rec);
+            Assert.assertEquals(rec.getValue(), Integer.toString(i + 1));
+        }
+
+        for (int i = 1; i <= 100; i++) {
+            shared.updateText(context, Integer.toString(i), Integer.toString(i + 2));
+        }
+
+        for (int i = 1; i <= 100; i++) {
+            StorageRecord rec = shared.readText(context, Integer.toString(i));
+            Assert.assertNotNull(rec);
+            Assert.assertEquals(rec.getValue(), Integer.toString(i + 2));
+        }
+
+        for (int i = 1; i <= 100; i++) {
+            boolean result = shared.createText(context, Integer.toString(i), Integer.toString(i + 1));
+            Assert.assertFalse(result, "createText should have failed");
+        }        
+        
+        for (int i = 1; i <= 100; i++) {
+            shared.deleteText(context, Integer.toString(i));
+            StorageRecord rec = shared.readText(context, Integer.toString(i));
+            Assert.assertNull(rec);
+        }
+    }
+}
---
+++ opensaml-util/src/test/java/org/opensaml/util/storage/impl/MemoryStorageServiceTest.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.util.storage.impl;
+
+import java.util.Timer;
+
+import javax.annotation.Nonnull;
+
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+
+import org.opensaml.util.storage.StorageService;
+import org.opensaml.util.storage.StorageServiceTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Test of {@link MemoryStorageService} implementation.
+ */
+public class MemoryStorageServiceTest extends StorageServiceTest {
+
+    /** {@inheritDoc} */
+    @Nonnull protected StorageService getStorageService() {
+        MemoryStorageService ss = new MemoryStorageService();
+        ss.setId("mem");
+        ss.setCleanupInterval(1000);
+        ss.setCleanupTaskTimer(new Timer());
+        return ss;
+    }
+        
+    @Test
+    public void invalidConfig() {
+        MemoryStorageService ss = new MemoryStorageService();
+        ss.setId("foo");
+        
+        try {
+            ss.initialize();
+            Assert.fail("Storage service should have failed to initialize");
+        } catch (ComponentInitializationException e) {
+            // expected
+        }
+        
+        ss.destroy();
+    }
+    
+    @Test
+    public void validConfig() throws ComponentInitializationException {
+        MemoryStorageService ss = new MemoryStorageService();
+        ss.setId("foo");
+        ss.setCleanupTaskTimer(new Timer());
+        
+        ss.initialize();
+        ss.destroy();
+    }
+    
+}
