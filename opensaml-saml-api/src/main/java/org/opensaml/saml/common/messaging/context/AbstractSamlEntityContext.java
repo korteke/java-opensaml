c8fe8aa2103c6b3ae1be1d62c1b211210d22dda9
putmanb <putmanb@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2013-05-11 01:55:15 +0000

    Add some view-like support to SAML peer entity context for dynamically resolving the entity ID from the message context.

--- opensaml-saml-api/src/main/java/org/opensaml/saml/common/messaging/context/AbstractSamlEntityContext.java
+++ opensaml-saml-api/src/main/java/org/opensaml/saml/common/messaging/context/AbstractSamlEntityContext.java
@@ -21,6 +21,7 @@ import javax.annotation.Nullable;
 import javax.xml.namespace.QName;
 
 import net.shibboleth.utilities.java.support.annotation.constraint.NotEmpty;
+import net.shibboleth.utilities.java.support.primitive.StringSupport;
 
 import org.opensaml.messaging.context.BaseContext;
 
@@ -51,7 +52,7 @@ public abstract class AbstractSamlEntityContext extends BaseContext {
      * @param id the new entityId
      */
     public void setEntityId(@Nullable final String id) {
-        entityId = id;
+        entityId = StringSupport.trimOrNull(id);
     }
 
     /**
