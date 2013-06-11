2a7b88fac846d2e89546b347783b8238c0115635
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2013-06-11 17:48:54 +0000

    Streamline storage API, add annotation support.

--- trunk/opensaml-util/src/main/java/org/opensaml/util/storage/StorageService.java
+++ trunk/opensaml-util/src/main/java/org/opensaml/util/storage/StorageService.java
@@ -43,7 +43,8 @@ import net.shibboleth.utilities.java.support.component.ValidatableComponent;
  * <p>The allowable sizes for contexts and keys can vary and be reported
  * by the implementation to callers, but MUST be at least 255 characters.</p>
  * 
- * <p>Expiration is expressed in milliseconds since the beginning of the epoch.</p>
+ * <p>Expiration is expressed in milliseconds since the beginning of the epoch,
+ * or a null can be used to signify no expiration.</p>
  */
 @ThreadSafeAfterInit
 public interface StorageService extends InitializableComponent, DestructableComponent,
@@ -57,46 +58,18 @@ public interface StorageService extends InitializableComponent, DestructableComp
     @Nonnull public StorageCapabilities getCapabilities();
 
     /**
-     * Creates a new record in the store with no explicit expiration.
-     * 
-     * @param context       a storage context label
-     * @param key           a key unique to context
-     * @param value         value to store
-     * 
-     * @return  true iff record was inserted, false iff a duplicate was found
-     * @throws IOException  if fatal errors occur in the insertion process 
-     */
-    public boolean create(@Nonnull @NotEmpty final String context, @Nonnull @NotEmpty final String key,
-            @Nonnull @NotEmpty final String value) throws IOException;
-    
-    /**
      * Creates a new record in the store with an expiration.
      * 
      * @param context       a storage context label
      * @param key           a key unique to context
      * @param value         value to store
-     * @param expiration    expiration for record
+     * @param expiration    expiration for record, or null
      * 
      * @return  true iff record was inserted, false iff a duplicate was found
-     * @throws IOException  if fatal errors occur in the insertion process 
+     * @throws IOException  if fatal errors occur in the insertion process
      */
     public boolean create(@Nonnull @NotEmpty final String context, @Nonnull @NotEmpty final String key,
-            @Nonnull @NotEmpty final String value, final long expiration) throws IOException;
-
-    /**
-     * Creates a new record in the store with no explicit expiration, using a custom serialization
-     * process for an arbitrary object.
-     * 
-     * @param context       a storage context label
-     * @param key           a key unique to context
-     * @param value         object to store
-     * @param serializer    custom serializer for the object
-     * 
-     * @return  true iff record was inserted, false iff a duplicate was found
-     * @throws IOException  if fatal errors occur in the insertion process 
-     */
-    public boolean create(@Nonnull @NotEmpty final String context, @Nonnull @NotEmpty final String key,
-            @Nonnull final Object value, @Nonnull final StorageSerializer serializer) throws IOException;
+            @Nonnull @NotEmpty final String value, @Nullable Long expiration) throws IOException;
 
     /**
      * Creates a new record in the store with an expiration, using a custom serialization
@@ -106,15 +79,28 @@ public interface StorageService extends InitializableComponent, DestructableComp
      * @param key           a key unique to context
      * @param value         object to store
      * @param serializer    custom serializer for the object
-     * @param expiration    expiration for record
+     * @param expiration    expiration for record, or null
      * 
      * @return  true iff record was inserted, false iff a duplicate was found
-     * @throws IOException  if fatal errors occur in the insertion process 
+     * @throws IOException  if fatal errors occur in the insertion process
      */
     public boolean create(@Nonnull @NotEmpty final String context, @Nonnull @NotEmpty final String key,
-            @Nonnull final Object value, @Nonnull final StorageSerializer serializer, final long expiration)
+            @Nonnull final Object value, @Nonnull final StorageSerializer serializer, @Nullable final Long expiration)
                     throws IOException;
     
+    /**
+     * Creates a new record in the store using an annotated object as the source.
+     * 
+     * <p>The individual parameters for the creation are extracted from the object using the
+     * annotations in the org.opensaml.util.storage.annotation package. If any are missing, or
+     * a field inaccessible, a runtime exception of some kind will occur.</p>
+     * 
+     * @param value         object to store
+     * 
+     * @return  true iff record was inserted, false iff a duplicate was found
+     * @throws IOException  if fatal errors occur in the insertion process
+     */
+    public boolean create(@Nonnull final Object value) throws IOException;
     
     /**
      * Returns an existing record from the store, if one exists.
@@ -127,6 +113,22 @@ public interface StorageService extends InitializableComponent, DestructableComp
      */
     @Nullable public StorageRecord read(@Nonnull @NotEmpty final String context,
             @Nonnull @NotEmpty final String key) throws IOException;
+
+    /**
+     * Returns an existing record from the store, if one exists, and uses it to
+     * update the annotated fields of a target object.
+     * 
+     * <p>The context and key to look up are obtained from the target object, and the
+     * value and expiration are written back, using the annotations in the
+     * org.opensaml.util.storage.annotation package. If any are missing, or
+     * a field inaccessible, a runtime exception of some kind will occur.</p>
+     *
+     * @param value         object to look up and populate
+     * 
+     * @return  the updated object passed into the method, or null if no record was found 
+     * @throws IOException  if errors occur in the read process 
+     */
+    @Nullable public Object read(@Nonnull final Object value) throws IOException;
     
     /**
      * Returns an existing record from the store, along with its version.
@@ -148,40 +150,28 @@ public interface StorageService extends InitializableComponent, DestructableComp
     
     
     /**
-     * Updates an existing record in the store, with no explicit expiration.
+     * Updates an existing record in the store.
      * 
      * @param context       a storage context label
      * @param key           a key unique to context
      * @param value         updated value
-     * 
-     * @return the version of the record after update, null if no record exists
-     * @throws IOException  if errors occur in the update process 
-     */
-    @Nullable public Integer update(@Nonnull @NotEmpty final String context,
-            @Nonnull @NotEmpty final String key, @Nonnull @NotEmpty final String value) throws IOException;
-
-    /**
-     * Updates an existing record in the store, and sets its expiration.
-     * 
-     * @param context       a storage context label
-     * @param key           a key unique to context
-     * @param value         updated value
-     * @param expiration    expiration for record
+     * @param expiration    expiration for record, or null
      * 
      * @return the version of the record after update, null if no record exists
      * @throws IOException  if errors occur in the update process 
      */
     @Nullable public Integer update(@Nonnull @NotEmpty final String context,
             @Nonnull @NotEmpty final String key, @Nonnull @NotEmpty final String value,
-            final long expiration) throws IOException;
+            @Nullable final Long expiration) throws IOException;
     
     /**
-     * Updates an existing record in the store with no explicit expiration, if a version matches.
+     * Updates an existing record in the store, if a version matches.
      * 
      * @param version       only update if the current version matches this value
      * @param context       a storage context label
      * @param key           a key unique to context
      * @param value         updated value
+     * @param expiration    expiration for record, or null
      * 
      * @return the version of the record after update, null if no record exists
      * @throws IOException  if errors occur in the update process
@@ -189,68 +179,34 @@ public interface StorageService extends InitializableComponent, DestructableComp
      */
     @Nullable public Integer updateWithVersion(final int version,
             @Nonnull @NotEmpty final String context, @Nonnull @NotEmpty final String key,
-            @Nonnull @NotEmpty final String value) throws IOException, VersionMismatchException;
-
-    /**
-     * Updates an existing record in the store and sets its expiration, if a version matches.
-     * 
-     * @param version       only update if the current version matches this value
-     * @param context       a storage context label
-     * @param key           a key unique to context
-     * @param value         updated value
-     * @param expiration    expiration for record
-     * 
-     * @return the version of the record after update, null if no record exists
-     * @throws IOException  if errors occur in the update process
-     * @throws VersionMismatchException if the record has already been updated to a newer version
-     */
-    @Nullable public Integer updateWithVersion(final int version,
-            @Nonnull @NotEmpty final String context, @Nonnull @NotEmpty final String key,
-            @Nonnull @NotEmpty final String value, final long expiration) throws IOException,
+            @Nonnull @NotEmpty final String value, @Nullable final Long expiration) throws IOException,
             VersionMismatchException;
 
     /**
-     * Updates an existing record in the store, with no explicit expiration, using a custom
-     * serialization strategy.
+     * Updates an existing record in the store using a custom serialization strategy.
      * 
      * @param context       a storage context label
      * @param key           a key unique to context
      * @param value         updated value
      * @param serializer    custom serializer
+     * @param expiration    expiration for record, or null
      * 
      * @return the version of the record after update, null if no record exists
      * @throws IOException  if errors occur in the update process 
      */
     @Nullable public Integer update(@Nonnull @NotEmpty final String context,
             @Nonnull @NotEmpty final String key, @Nonnull final Object value,
-            @Nonnull final StorageSerializer serializer) throws IOException;
-
-    /**
-     * Updates an existing record in the store, and sets its expiration, using a custom
-     * serialization strategy.
-     * 
-     * @param context       a storage context label
-     * @param key           a key unique to context
-     * @param value         updated value
-     * @param serializer    custom serializer
-     * @param expiration    expiration for record
-     * 
-     * @return the version of the record after update, null if no record exists
-     * @throws IOException  if errors occur in the update process 
-     */
-    @Nullable public Integer update(@Nonnull @NotEmpty final String context,
-            @Nonnull @NotEmpty final String key, @Nonnull final Object value,
-            @Nonnull final StorageSerializer serializer, final long expiration) throws IOException;
+            @Nonnull final StorageSerializer serializer, @Nullable final Long expiration) throws IOException;
     
     /**
-     * Updates an existing record in the store with no explicit expiration, if a version matches,
-     * using a custom serialization strategy.
+     * Updates an existing record in the store, if a version matches, using a custom serialization strategy.
      * 
      * @param version       only update if the current version matches this value
      * @param context       a storage context label
      * @param key           a key unique to context
      * @param value         updated value
      * @param serializer    custom serializer
+     * @param expiration    expiration for record, or null
      * 
      * @return the version of the record after update, null if no record exists
      * @throws IOException  if errors occur in the update process
@@ -258,52 +214,66 @@ public interface StorageService extends InitializableComponent, DestructableComp
      */
     @Nullable public Integer updateWithVersion(final int version, @Nonnull @NotEmpty final String context,
             @Nonnull @NotEmpty final String key, @Nonnull final Object value,
-            @Nonnull final StorageSerializer serializer) throws IOException, VersionMismatchException;
-
-    /**
-     * Updates an existing record in the store and sets its expiration, if a version matches,
-     * using a custom serialization strategy.
-     * 
-     * @param version       only update if the current version matches this value
-     * @param context       a storage context label
-     * @param key           a key unique to context
-     * @param value         updated value
-     * @param serializer    custom serializer
-     * @param expiration    expiration for record
-     * 
-     * @return the version of the record after update, null if no record exists
-     * @throws IOException  if errors occur in the update process
-     * @throws VersionMismatchException if the record has already been updated to a newer version
-     */
-    @Nullable public Integer updateWithVersion(final int version, @Nonnull @NotEmpty final String context,
-            @Nonnull @NotEmpty final String key, @Nonnull final Object value,
-            @Nonnull final StorageSerializer serializer, final long expiration)
+            @Nonnull final StorageSerializer serializer, @Nullable final Long expiration)
                     throws IOException, VersionMismatchException;
-    
+
     /**
-     * Updates expiration of an existing record in the store to no expiration.
+     * Updates an existing record in the store, using an annotated object as the source.
      * 
-     * @param context       a storage context label
-     * @param key           a key unique to context
+     * <p>The individual parameters for the update are extracted from the object using the
+     * annotations in the org.opensaml.util.storage.annotation package. If any are missing, or
+     * a field inaccessible, a runtime exception of some kind will occur.</p>
      * 
-     * @return the version of the record, null if no record exists
+     * @param value         object to update from
+     * 
+     * @return the version of the record after update, null if no record exists
      * @throws IOException  if errors occur in the update process 
      */
-    @Nullable public Integer updateExpiration(@Nonnull @NotEmpty final String context,
-            @Nonnull @NotEmpty final String key) throws IOException;
-
+    @Nullable public Integer update(@Nonnull final Object value) throws IOException;
+    
+    /**
+     * Updates an existing record in the store, if a version matches, using an annotated object as the source.
+     * 
+     * <p>The individual parameters for the update are extracted from the object using the
+     * annotations in the org.opensaml.util.storage.annotation package. If any are missing, or
+     * a field inaccessible, a runtime exception of some kind will occur.</p>
+     * 
+     * @param version       only update if the current version matches this value
+     * @param value         object to update from
+     * 
+     * @return the version of the record after update, null if no record exists
+     * @throws IOException  if errors occur in the update process
+     * @throws VersionMismatchException if the record has already been updated to a newer version
+     */
+    @Nullable public Integer updateWithVersion(final int version, @Nonnull final Object value)
+            throws IOException, VersionMismatchException;
+    
     /**
      * Updates expiration of an existing record in the store.
      * 
      * @param context       a storage context label
      * @param key           a key unique to context
-     * @param expiration    expiration for record
+     * @param expiration    expiration for record, or null
      * 
      * @return the version of the record, null if no record exists
      * @throws IOException  if errors occur in the update process 
      */
     @Nullable public Integer updateExpiration(@Nonnull @NotEmpty final String context,
-            @Nonnull @NotEmpty final String key, final long expiration) throws IOException;
+            @Nonnull @NotEmpty final String key, @Nullable final Long expiration) throws IOException;
+
+    /**
+     * Updates expiration of an existing record in the store, using an annotated object as the source.
+     * 
+     * <p>The individual parameters for the update are extracted from the object using the
+     * annotations in the org.opensaml.util.storage.annotation package. If any are missing, or
+     * a field inaccessible, a runtime exception of some kind will occur.</p>
+     * 
+     * @param value         object to update from
+     * 
+     * @return the version of the record, null if no record exists
+     * @throws IOException  if errors occur in the update process 
+     */
+    @Nullable public Integer updateExpiration(@Nonnull final Object value) throws IOException;
     
     
     /**
@@ -317,6 +287,20 @@ public interface StorageService extends InitializableComponent, DestructableComp
      */
     public boolean delete(@Nonnull @NotEmpty final String context, @Nonnull @NotEmpty final String key)
             throws IOException;    
+
+    /**
+     * Deletes an existing record from the store, using an annotated object as the source.
+     * 
+     * <p>The individual parameters for the deletion are extracted from the object using the
+     * annotations in the org.opensaml.util.storage.annotation package. If any are missing, or
+     * a field inaccessible, a runtime exception of some kind will occur.</p>
+     * 
+     * @param value         object to delete
+     * 
+     * @return true iff the record existed and was deleted
+     * @throws IOException  if errors occur in the deletion process 
+     */
+    public boolean delete(@Nonnull final Object value) throws IOException;    
     
     /**
      * Manually trigger a cleanup of expired records. The method <strong>MAY</strong> return without guaranteeing
@@ -329,23 +313,14 @@ public interface StorageService extends InitializableComponent, DestructableComp
     public void reap(@Nonnull @NotEmpty final String context) throws IOException;
     
     /**
-     * Updates the expiration time of all records in the context to no expiration.
-     * 
-     * @param context       a storage context label
-     * 
-     * @throws IOException  if errors occur in the cleanup process
-     */
-    public void updateContextExpiration(@Nonnull @NotEmpty final String context) throws IOException;
-    
-    /**
      * Updates the expiration time of all records in the context.
      * 
      * @param context       a storage context label
-     * @param expiration    a new expiration timestamp
+     * @param expiration    a new expiration timestamp, or null
      * 
      * @throws IOException  if errors occur in the cleanup process
      */
-    public void updateContextExpiration(@Nonnull @NotEmpty final String context, final long expiration)
+    public void updateContextExpiration(@Nonnull @NotEmpty final String context, @Nullable final Long expiration)
             throws IOException;
 
     /**
