ad03f8d599a66db87d9f0d1d0e7b3ecfbd104a4b
putmanb <putmanb@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2013-09-13 23:21:58 +0000

    Metadata node processor for the node processing metadata filter which supports adding object metadata to an EntityDescriptor for each ancestor EntitiesDescriptor, adding object metadata for the latter's @Name attribute.

---
+++ opensaml-saml-api/src/main/java/org/opensaml/saml/metadata/EntitiesDescriptorGroupName.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.saml.metadata;
+
+import javax.annotation.Nonnull;
+
+import net.shibboleth.utilities.java.support.annotation.constraint.NotEmpty;
+import net.shibboleth.utilities.java.support.logic.Constraint;
+import net.shibboleth.utilities.java.support.primitive.StringSupport;
+
+import org.opensaml.core.xml.XMLObject;
+import org.opensaml.saml.saml2.metadata.EntityDescriptor;
+
+/**
+ * A bean class which is used to represent an entities descriptor group to which an 
+ * {@link EntityDescriptor} belongs.  It will typically be attached to an entity descriptor
+ * via its {@link XMLObject#getObjectMetadata()}.
+ */
+public class EntitiesDescriptorGroupName {
+    
+    /** The entities descriptor group name. */
+    private String name;
+    
+    /**
+     * Constructor.
+     *
+     * @param newName the entities descriptor group name
+     */
+    public EntitiesDescriptorGroupName(@Nonnull @NotEmpty final String newName) {
+        name = Constraint.isNotNull(StringSupport.trimOrNull(newName), 
+                "EntitiesDescriptor group name may not be null or empty");
+    }
+    
+    /**
+     * Get the entities descriptor group name.
+     * 
+     * @return the entities descriptor group name
+     */
+    @Nonnull @NotEmpty public String getName() {
+        return name;
+    }
+
+    /** {@inheritDoc} */
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    /** {@inheritDoc} */
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        
+        if (obj instanceof EntitiesDescriptorGroupName) {
+            return name.equals(((EntitiesDescriptorGroupName)obj).getName());
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ opensaml-saml-impl/src/main/java/org/opensaml/saml/metadata/resolver/filter/impl/EntitiesDescriptorNameProcessor.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.saml.metadata.resolver.filter.impl;
+
+import net.shibboleth.utilities.java.support.primitive.StringSupport;
+
+import org.opensaml.core.xml.XMLObject;
+import org.opensaml.saml.metadata.EntitiesDescriptorGroupName;
+import org.opensaml.saml.metadata.resolver.filter.FilterException;
+import org.opensaml.saml.metadata.resolver.filter.impl.NodeProcessingMetadataFilter.MetadataNodeProcessor;
+import org.opensaml.saml.saml2.metadata.EntitiesDescriptor;
+import org.opensaml.saml.saml2.metadata.EntityDescriptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A metadata node processor implementation which attaches an instance of
+ * {@link EntitiesDescriptorGroupName} to an {@link EntityDescriptor} for
+ * each ancestor {@link EntitiesDescriptor} in the metadata tree.
+ */
+public class EntitiesDescriptorNameProcessor implements MetadataNodeProcessor {
+    
+    /** Logger. */
+    private Logger log = LoggerFactory.getLogger(EntitiesDescriptorNameProcessor.class);
+
+    /** {@inheritDoc} */
+    public void process(XMLObject metadataNode) throws FilterException {
+        if (metadataNode instanceof EntityDescriptor) {
+            XMLObject currentParent = metadataNode.getParent();
+            while (currentParent != null) {
+                if (currentParent instanceof EntitiesDescriptor) {
+                    String name = StringSupport.trimOrNull(((EntitiesDescriptor)currentParent).getName());
+                    if (name != null) {
+                        if (log.isDebugEnabled()) {
+                            log.debug("Attaching EntitiesDescriptor group name '{}' to EntityDescriptor: {}", 
+                                    name, ((EntityDescriptor)metadataNode).getEntityID());
+                        }
+                        metadataNode.getObjectMetadata().put(new EntitiesDescriptorGroupName(name));
+                    }
+                }
+                currentParent = currentParent.getParent();
+            }
+        }
+    }
+
+}
