439d0b24a9b061124188880069afce84970b60b8
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2013-12-16 20:39:29 +0000

    Add SAML 1 Authn Method constants

--- opensaml-saml-api/src/main/java/org/opensaml/saml/saml1/core/AuthenticationStatement.java
+++ opensaml-saml-api/src/main/java/org/opensaml/saml/saml1/core/AuthenticationStatement.java
@@ -31,43 +31,77 @@ import org.opensaml.saml.common.xml.SAMLConstants;
 public interface AuthenticationStatement extends SAMLObject, SubjectStatement {
 
     /** Element name, no namespace. */
-    public final static String DEFAULT_ELEMENT_LOCAL_NAME = "AuthenticationStatement";
+    public static final String DEFAULT_ELEMENT_LOCAL_NAME = "AuthenticationStatement";
     
-    /** Default element name */
-    public final static QName DEFAULT_ELEMENT_NAME = new QName(SAMLConstants.SAML1_NS, DEFAULT_ELEMENT_LOCAL_NAME, SAMLConstants.SAML1_PREFIX);
+    /** Default element name. */
+    public static final QName DEFAULT_ELEMENT_NAME =
+            new QName(SAMLConstants.SAML1_NS, DEFAULT_ELEMENT_LOCAL_NAME, SAMLConstants.SAML1_PREFIX);
     
-    /** Local name of the XSI type */
-    public final static String TYPE_LOCAL_NAME = "AuthenticationStatementType"; 
+    /** Local name of the XSI type. */
+    public static final String TYPE_LOCAL_NAME = "AuthenticationStatementType"; 
         
-    /** QName of the XSI type */
-    public final static QName TYPE_NAME = new QName(SAMLConstants.SAML1_NS, TYPE_LOCAL_NAME, SAMLConstants.SAML1_PREFIX);
+    /** QName of the XSI type. */
+    public static final QName TYPE_NAME =
+            new QName(SAMLConstants.SAML1_NS, TYPE_LOCAL_NAME, SAMLConstants.SAML1_PREFIX);
 
-    /** Name of the AuthenticationMethod attribute */
-    public final static String AUTHENTICATIONMETHOD_ATTRIB_NAME = "AuthenticationMethod";
+    /** Name of the AuthenticationMethod attribute. */
+    public static final String AUTHENTICATIONMETHOD_ATTRIB_NAME = "AuthenticationMethod";
 
-    /** Name of the AuthenticationInstant attribute */
-    public final static String AUTHENTICATIONINSTANT_ATTRIB_NAME = "AuthenticationInstant";
+    /** Name of the AuthenticationInstant attribute. */
+    public static final String AUTHENTICATIONINSTANT_ATTRIB_NAME = "AuthenticationInstant";
 
-    /** Return the contents of the AuthenticationMethod attribute */
+    /** URI for Kerberos authentication method. */
+    public static final String KERBEROS_AUTHN_METHOD = "urn:ietf:rfc:1510";
+    
+    /** URI for Hardware Token authentication method. */
+    public static final String HARDWARE_TOKEN_AUTHN_METHOD = "urn:oasis:names:tc:SAML:1.0:am:HardwareToken";
+
+    /** URI for Password authentication method. */
+    public static final String PASSWORD_AUTHN_METHOD = "urn:oasis:names:tc:SAML:1.0:am:password";
+
+    /** URI for X509 Public Key authentication method. */
+    public static final String X509_AUTHN_METHOD = "urn:oasis:names:tc:SAML:1.0:am:X509-PKI";
+
+    /** URI for PGP authentication method. */
+    public static final String PGP_AUTHN_METHOD = "urn:oasis:names:tc:SAML:1.0:am:PGP";
+
+    /** URI for SPKI authentication method. */
+    public static final String SPKI_AUTHN_METHOD = "urn:oasis:names:tc:SAML:1.0:am:SPKI";
+
+    /** URI for XKMS authentication method. */
+    public static final String XKMS_AUTHN_METHOD = "urn:oasis:names:tc:SAML:1.0:am:XKMS";
+    
+    /** URI for XML Digital Signature authentication method. */
+    public static final String XML_DSIG_AUTHN_METHOD = "urn:ietf:rfc:3075";
+
+    /** URI for Secure Remote Password authentication method. */
+    public static final String SRP_AUTHN_METHOD = "urn:ietf:rfc:2945";
+
+    /** URI for SSL/TLS Client authentication method. */
+    public static final String TLS_CLIENT_AUTHN_METHOD = "urn:ietf:rfc:2246";
+
+    /** URI for unspecified authentication method. */
+    public static final String UNSPECIFIED_AUTHN_METHOD = "urn:oasis:names:tc:SAML:1.0:am:unspecified";    
+    
+    /** Return the contents of the AuthenticationMethod attribute. */
     public String getAuthenticationMethod();
 
-    /** Set the contents of the AuthenticationMethod attribute */
+    /** Set the contents of the AuthenticationMethod attribute. */
     public void setAuthenticationMethod(String authenticationMethod);
 
-    /** Return the contents of the AuthenticationInstant attribute */
+    /** Return the contents of the AuthenticationInstant attribute. */
     public DateTime getAuthenticationInstant();
 
-    /** Set the contents of the AuthenticationInstant attribute */
+    /** Set the contents of the AuthenticationInstant attribute. */
     public void setAuthenticationInstant(DateTime authenticationInstant);
 
-    /** Set the (single) SubjectLocality child element */
+    /** Set the (single) SubjectLocality child element. */
     public SubjectLocality getSubjectLocality();
 
-    /** Get the (single) SubjectLocality child element 
-     * @throws IllegalArgumentException */
+    /** Get the (single) SubjectLocality child element. */
     public void setSubjectLocality(SubjectLocality subjectLocality) throws IllegalArgumentException;
 
-    /** return all the AuthorityBinding subelement */
+    /** return all the AuthorityBinding subelement. */
     public List<AuthorityBinding> getAuthorityBindings();
 
 }
