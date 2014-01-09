6394e60a00f31ff62b9decbb1f313dbefb2c688a
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2014-01-09 03:12:08 +0000

    Holder for SAML Consent values.

---
+++ trunk/opensaml-saml-api/src/main/java/org/opensaml/saml/common/messaging/context/SAMLConsentContext.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.saml.common.messaging.context;
+
+import javax.annotation.Nullable;
+
+import net.shibboleth.utilities.java.support.annotation.constraint.NotEmpty;
+import net.shibboleth.utilities.java.support.primitive.StringSupport;
+
+import org.opensaml.messaging.context.BaseContext;
+
+/**
+ * Subcontext that carries information about a SAML Consent value.
+ */
+public class SAMLConsentContext extends BaseContext {
+
+    /** The SAML Consent value in use. */
+    @Nullable @NotEmpty private String value;
+
+    /**
+     * Get the SAML Consent value in use.
+     * 
+     * @return SAML Consent value in use
+     */
+    @Nullable @NotEmpty public String getConsent() {
+        return value;
+    }
+
+    /**
+     * Set the SAML Consent value in use.
+     * 
+     * @param consent SAML Consent value in use
+     */
+    public void setConsent(@Nullable final String consent) {
+        value = StringSupport.trimOrNull(consent);
+    }
+
+}
