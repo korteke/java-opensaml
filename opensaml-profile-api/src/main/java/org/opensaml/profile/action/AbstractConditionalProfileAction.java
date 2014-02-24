fd0c92b8e12fc8388ea3a7d2624186904641b278
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2014-02-24 19:42:15 +0000

    Add a base class for conditional profile actions.

---
+++ opensaml-profile-api/src/main/java/org/opensaml/profile/action/AbstractConditionalProfileAction.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.profile.action;
+
+import javax.annotation.Nonnull;
+
+import org.opensaml.profile.ProfileException;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+
+/**
+ * Base class for profile actions.
+ * 
+ * @param <InboundMessageType> type of in-bound message
+ * @param <OutboundMessageType> type of out-bound message
+ */
+public abstract class AbstractConditionalProfileAction<InboundMessageType, OutboundMessageType> extends
+        AbstractProfileAction<InboundMessageType, OutboundMessageType> {
+    
+    /** Condition dictating whether to run or not. */
+    @Nonnull private Predicate<ProfileRequestContext> activationCondition;
+    
+    /** Constructor. */
+    public AbstractConditionalProfileAction() {
+        activationCondition = Predicates.alwaysTrue();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected boolean doPreExecute(
+            @Nonnull final ProfileRequestContext<InboundMessageType, OutboundMessageType> profileRequestContext)
+            throws ProfileException {
+        if (activationCondition.apply(profileRequestContext)) {
+            return super.doPreExecute(profileRequestContext);
+        } else {
+            LoggerFactory.getLogger(AbstractConditionalProfileAction.class).debug(
+                    "{} Activation condition for action returned false", getLogPrefix());
+            return false;
+        }
+    }
+
+}
