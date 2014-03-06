6163e66bb77adc2fa4d92fdcbabd980a41c69306
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2014-03-06 03:21:37 +0000

    IDP-124 - create dual version action

--- trunk/opensaml-saml-impl/src/test/java/org/opensaml/saml/common/profile/impl/AddNotOnOrAfterConditionToAssertionsTest.java
+++ trunk/opensaml-saml-impl/src/test/java/org/opensaml/saml/common/profile/impl/AddNotOnOrAfterConditionToAssertionsTest.java
@@ -15,20 +15,24 @@
  * limitations under the License.
  */
 
-package org.opensaml.saml.saml1.profile.impl;
+package org.opensaml.saml.common.profile.impl;
+
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
 
 import org.opensaml.core.OpenSAMLInitBaseTestCase;
 import org.opensaml.core.xml.config.XMLObjectProviderRegistrySupport;
+import org.opensaml.profile.ProfileException;
 import org.opensaml.profile.RequestContextBuilder;
 import org.opensaml.profile.action.ActionTestingSupport;
 import org.opensaml.profile.action.EventIds;
 import org.opensaml.profile.context.ProfileRequestContext;
 import org.opensaml.saml.common.SAMLObjectBuilder;
+import org.opensaml.saml.common.profile.impl.AddNotOnOrAfterConditionToAssertions;
 import org.opensaml.saml.saml1.core.Assertion;
 import org.opensaml.saml.saml1.core.Conditions;
 import org.opensaml.saml.saml1.core.Response;
 import org.opensaml.saml.saml1.profile.SAML1ActionTestingSupport;
-import org.opensaml.saml.saml1.profile.impl.AddNotOnOrAfterConditionToAssertions;
+import org.opensaml.saml.saml2.profile.SAML2ActionTestingSupport;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -40,20 +44,22 @@ public class AddNotOnOrAfterConditionToAssertionsTest  extends OpenSAMLInitBaseT
 
     private ProfileRequestContext prc;
     
+    private AddNotOnOrAfterConditionToAssertions action;
+    
     @BeforeMethod
-    public void setUp() {
+    public void setUp() throws ComponentInitializationException {
         prc = new RequestContextBuilder().setOutboundMessage(
                 SAML1ActionTestingSupport.buildResponse()).buildProfileRequestContext();
+        
+        action = new AddNotOnOrAfterConditionToAssertions();
+        action.setId("test");
+        action.initialize();
     }
     
     /** Test that action errors out properly if there is no response. */
     @Test
-    public void testNoResponse() throws Exception {
+    public void testNoResponse() throws ProfileException {
         prc.setOutboundMessageContext(null);
-        
-        final AddNotOnOrAfterConditionToAssertions action = new AddNotOnOrAfterConditionToAssertions();
-        action.setId("test");
-        action.initialize();
 
         action.execute(prc);
         ActionTestingSupport.assertEvent(prc, EventIds.INVALID_MSG_CTX);
@@ -61,10 +67,7 @@ public class AddNotOnOrAfterConditionToAssertionsTest  extends OpenSAMLInitBaseT
 
     /** Test that action works properly if there is no assertion in the response. */
     @Test
-    public void testNoAssertion() throws Exception {
-        final AddNotBeforeConditionToAssertions action = new AddNotBeforeConditionToAssertions();
-        action.setId("test");
-        action.initialize();
+    public void testNoAssertion() throws ProfileException {
 
         action.execute(prc);
         ActionTestingSupport.assertProceedEvent(prc);
@@ -75,15 +78,11 @@ public class AddNotOnOrAfterConditionToAssertionsTest  extends OpenSAMLInitBaseT
      * response.
      */
     @Test
-    public void testSingleAssertion() throws Exception {
+    public void testSingleAssertion() throws ProfileException {
         final Assertion assertion = SAML1ActionTestingSupport.buildAssertion();
 
         final Response response = (Response) prc.getOutboundMessageContext().getMessage();
         response.getAssertions().add(assertion);
-
-        final AddNotOnOrAfterConditionToAssertions action = new AddNotOnOrAfterConditionToAssertions();
-        action.setId("test");
-        action.initialize();
 
         action.execute(prc);
         ActionTestingSupport.assertProceedEvent(prc);
@@ -103,8 +102,8 @@ public class AddNotOnOrAfterConditionToAssertionsTest  extends OpenSAMLInitBaseT
      * response.
      */
     @Test
-    public void testSingleAssertionWithExistingConditions() throws Exception {
-        SAMLObjectBuilder<Conditions> conditionsBuilder = (SAMLObjectBuilder<Conditions>)
+    public void testSingleAssertionWithExistingConditions() throws ProfileException, ComponentInitializationException {
+        final SAMLObjectBuilder<Conditions> conditionsBuilder = (SAMLObjectBuilder<Conditions>)
                 XMLObjectProviderRegistrySupport.getBuilderFactory().getBuilder(Conditions.DEFAULT_ELEMENT_NAME);
         final Conditions conditions = conditionsBuilder.buildObject();
 
@@ -132,7 +131,7 @@ public class AddNotOnOrAfterConditionToAssertionsTest  extends OpenSAMLInitBaseT
 
     /** Test that the condition is properly added if there are multiple assertions in the response. */
     @Test
-    public void testMultipleAssertion() throws Exception {
+    public void testMultipleAssertion() throws ProfileException, ComponentInitializationException {
         final Response response = (Response) prc.getOutboundMessageContext().getMessage();
         response.getAssertions().add(SAML1ActionTestingSupport.buildAssertion());
         response.getAssertions().add(SAML1ActionTestingSupport.buildAssertion());
@@ -162,6 +161,30 @@ public class AddNotOnOrAfterConditionToAssertionsTest  extends OpenSAMLInitBaseT
                     assertion.getConditions().getNotOnOrAfter().minus(response.getIssueInstant().getMillis()).getMillis(),
                     3 * 60 * 1000);
         }
+    }
+
+    /**
+     * Test that the condition is properly added if there is a single assertion, without a Conditions element, in the
+     * response.
+     */
+    @Test
+    public void testSAML2Assertion() throws ProfileException {
+        final org.opensaml.saml.saml2.core.Assertion assertion = SAML2ActionTestingSupport.buildAssertion();
+        final org.opensaml.saml.saml2.core.Response response = SAML2ActionTestingSupport.buildResponse();
+        response.getAssertions().add(assertion);
+        prc.getOutboundMessageContext().setMessage(response);
+
+        action.execute(prc);
+        ActionTestingSupport.assertProceedEvent(prc);
+
+        Assert.assertNotNull(response.getAssertions());
+        Assert.assertEquals(response.getAssertions().size(), 1);
+
+        Assert.assertNotNull(assertion.getConditions());
+        Assert.assertNotNull(assertion.getConditions().getNotOnOrAfter());
+        Assert.assertEquals(
+                assertion.getConditions().getNotOnOrAfter().minus(response.getIssueInstant().getMillis()).getMillis(),
+                5 * 60 * 1000);
     }
     
 }
