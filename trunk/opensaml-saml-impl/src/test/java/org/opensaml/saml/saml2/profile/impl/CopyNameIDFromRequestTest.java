cb69d29bda058dc221a1b832d295fcc9440c33b9
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2014-04-07 14:51:30 +0000

    IDP-393 - copy NameID action

---
+++ trunk/opensaml-saml-impl/src/test/java/org/opensaml/saml/saml2/profile/impl/CopyNameIDFromRequestTest.java
@@ -0,0 +1,119 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.saml.saml2.profile.impl;
+
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+
+import org.opensaml.core.OpenSAMLInitBaseTestCase;
+import org.opensaml.profile.ProfileException;
+import org.opensaml.profile.RequestContextBuilder;
+import org.opensaml.profile.action.ActionTestingSupport;
+import org.opensaml.profile.action.EventIds;
+import org.opensaml.profile.context.ProfileRequestContext;
+import org.opensaml.saml.saml2.core.Assertion;
+import org.opensaml.saml.saml2.core.AttributeQuery;
+import org.opensaml.saml.saml2.core.Response;
+import org.opensaml.saml.saml2.core.Subject;
+import org.opensaml.saml.saml2.profile.SAML2ActionTestingSupport;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+/** Test for {@link CopyNameIDFromRequest}. */
+public class CopyNameIDFromRequestTest extends OpenSAMLInitBaseTestCase {
+
+    private static final String NAME_QUALIFIER = "https://idp.example.org";
+
+    private ProfileRequestContext<AttributeQuery,Response> prc;
+    
+    private CopyNameIDFromRequest action;
+    
+    @BeforeMethod
+    public void setUp() throws ComponentInitializationException {
+        prc = new RequestContextBuilder().buildProfileRequestContext();
+        
+        action = new CopyNameIDFromRequest();
+        action.setId("test");
+        action.initialize();
+    }
+    
+    @Test
+    public void testNoResponse() throws ComponentInitializationException, ProfileException {
+        action.execute(prc);
+        ActionTestingSupport.assertEvent(prc, EventIds.INVALID_MSG_CTX);
+    }
+
+    @Test
+    public void testNoAssertions() throws ComponentInitializationException, ProfileException {
+        prc.getOutboundMessageContext().setMessage(SAML2ActionTestingSupport.buildResponse());
+
+        action.execute(prc);
+        ActionTestingSupport.assertProceedEvent(prc);
+        Assert.assertTrue(prc.getOutboundMessageContext().getMessage().getAssertions().isEmpty());
+    }
+
+    @Test
+    public void testNoRequest() throws ProfileException {
+        prc.getOutboundMessageContext().setMessage(SAML2ActionTestingSupport.buildResponse());
+        prc.getOutboundMessageContext().getMessage().getAssertions().add(SAML2ActionTestingSupport.buildAssertion());
+        addAssertions();
+
+        action.execute(prc);
+        ActionTestingSupport.assertEvent(prc, EventIds.INVALID_MSG_CTX);
+    }
+
+    @Test
+    public void testNoName() throws ProfileException {
+        prc.getOutboundMessageContext().setMessage(SAML2ActionTestingSupport.buildResponse());
+        prc.getOutboundMessageContext().getMessage().getAssertions().add(SAML2ActionTestingSupport.buildAssertion());
+        addAssertions();
+
+        prc.getInboundMessageContext().setMessage(SAML2ActionTestingSupport.buildAttributeQueryRequest(null));
+        
+        action.execute(prc);
+        ActionTestingSupport.assertEvent(prc, EventIds.INVALID_MSG_CTX);
+    }
+    
+    @Test void testCopy() throws ComponentInitializationException, ProfileException {
+        
+        Subject subject = SAML2ActionTestingSupport.buildSubject("jdoe");
+        subject.getNameID().setNameQualifier(NAME_QUALIFIER);
+        prc.getInboundMessageContext().setMessage(SAML2ActionTestingSupport.buildAttributeQueryRequest(subject));
+        addAssertions();
+        
+        action.execute(prc);
+        ActionTestingSupport.assertProceedEvent(prc);
+        
+        Assertion assertion = prc.getOutboundMessageContext().getMessage().getAssertions().get(0);
+        subject = assertion.getSubject();
+        Assert.assertNotNull(subject);
+        Assert.assertNotNull(subject.getNameID());
+        Assert.assertEquals(subject.getNameID().getValue(), "jdoe");
+        Assert.assertEquals(subject.getNameID().getNameQualifier(), NAME_QUALIFIER);
+    }
+
+    /** Set up the test message with some assertions. */
+    private void addAssertions() {
+        final Response response = SAML2ActionTestingSupport.buildResponse();
+        response.getAssertions().add(SAML2ActionTestingSupport.buildAssertion());
+        response.getAssertions().add(SAML2ActionTestingSupport.buildAssertion());
+        prc.getOutboundMessageContext().setMessage(response);
+    }
+    
+}
