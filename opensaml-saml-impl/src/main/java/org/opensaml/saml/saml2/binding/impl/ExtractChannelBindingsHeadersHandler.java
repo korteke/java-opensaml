ffb3ba3d1fa98995d3040015c27f84dcd0bf8961
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2014-07-15 03:32:55 +0000

    IDP-414: SOAP header extraction of channel bindings

---
+++ opensaml-saml-impl/src/main/java/org/opensaml/saml/saml2/binding/impl/ExtractChannelBindingsHeadersHandler.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.saml.saml2.binding.impl;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+import org.opensaml.core.xml.XMLObject;
+import org.opensaml.messaging.context.MessageContext;
+import org.opensaml.messaging.handler.AbstractMessageHandler;
+import org.opensaml.messaging.handler.MessageHandlerException;
+import org.opensaml.saml.common.messaging.context.ChannelBindingsContext;
+import org.opensaml.saml.ext.saml2cb.ChannelBindings;
+import org.opensaml.soap.messaging.context.SOAP11Context;
+import org.opensaml.soap.util.SOAPSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Lists;
+
+/**
+ * MessageHandler to process {@link ChannelBindings} SOAP header blocks in an incoming SOAP envelope
+ * and save them to a {@link ChannelBindingsContext} underneath the {@link SOAP11Context}.
+ */
+public class ExtractChannelBindingsHeadersHandler extends AbstractMessageHandler {
+   
+    /** Class logger. */
+    @Nonnull private final Logger log = LoggerFactory.getLogger(ExtractChannelBindingsHeadersHandler.class);
+    
+    /** {@inheritDoc} */
+    @Override
+    protected void doInvoke(@Nonnull final MessageContext messageContext) throws MessageHandlerException {
+        
+        final Collection<ChannelBindings> channelBindings = Lists.newArrayList();
+        
+        final List<XMLObject> headers =
+                SOAPSupport.getInboundHeaderBlock(messageContext, ChannelBindings.DEFAULT_ELEMENT_NAME, null, true);
+        for (final XMLObject header : headers) {
+            if (header instanceof ChannelBindings) {
+                channelBindings.add((ChannelBindings) header);
+            }
+        }
+        
+        if (channelBindings.isEmpty()) {
+            log.debug("{} No ChannelBindings header blocks found", getLogPrefix());
+        } else {
+            log.debug("{} {} ChannelBindings header block(s) found", getLogPrefix(), channelBindings.size());
+            messageContext.getSubcontext(SOAP11Context.class).getSubcontext(ChannelBindingsContext.class,
+                    true).getChannelBindings().addAll(channelBindings);
+        }
+    }
+    
+}
