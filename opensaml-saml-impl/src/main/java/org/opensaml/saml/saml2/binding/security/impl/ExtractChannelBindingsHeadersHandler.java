bb6e3e5d8ae2cdcd1c9d2f30bee56089c4d06c34
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2014-07-15 21:19:53 +0000

    IDP-414: Add actor filtering to extraction.

--- opensaml-saml-impl/src/main/java/org/opensaml/saml/saml2/binding/security/impl/ExtractChannelBindingsHeadersHandler.java
+++ opensaml-saml-impl/src/main/java/org/opensaml/saml/saml2/binding/security/impl/ExtractChannelBindingsHeadersHandler.java
@@ -22,6 +22,8 @@ import java.util.List;
 
 import javax.annotation.Nonnull;
 
+import net.shibboleth.utilities.java.support.component.ComponentSupport;
+
 import org.opensaml.core.xml.XMLObject;
 import org.opensaml.messaging.context.MessageContext;
 import org.opensaml.messaging.handler.AbstractMessageHandler;
@@ -29,6 +31,7 @@ import org.opensaml.messaging.handler.MessageHandlerException;
 import org.opensaml.saml.common.messaging.context.ChannelBindingsContext;
 import org.opensaml.saml.ext.saml2cb.ChannelBindings;
 import org.opensaml.soap.messaging.context.SOAP11Context;
+import org.opensaml.soap.soap11.ActorBearing;
 import org.opensaml.soap.util.SOAPSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -44,17 +47,52 @@ public class ExtractChannelBindingsHeadersHandler extends AbstractMessageHandler
     /** Class logger. */
     @Nonnull private final Logger log = LoggerFactory.getLogger(ExtractChannelBindingsHeadersHandler.class);
     
+    /** Include header blocks targeted at the final destination node? */
+    private boolean finalDestination;
+    
+    /** Include header blocks targeted at the next destination node? */
+    private boolean nextDestination;
+    
+    /** Constructor. */
+    public ExtractChannelBindingsHeadersHandler() {
+        nextDestination = true;
+    }
+    
+    /**
+     * Set whether to include header blocks targeted at the final destination node (defaults to false).
+     * 
+     * @param flag flag to set
+     */
+    public void setFinalDestination(final boolean flag) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+        
+        finalDestination = flag;
+    }
+
+    /**
+     * Set whether to include header blocks targeted at the next destination node (defaults to true).
+     * 
+     * @param flag flag to set
+     */
+    public void setNextDestination(final boolean flag) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+        
+        nextDestination = flag;
+    }
+    
     /** {@inheritDoc} */
     @Override
     protected void doInvoke(@Nonnull final MessageContext messageContext) throws MessageHandlerException {
         
         final Collection<ChannelBindings> channelBindings = Lists.newArrayList();
         
-        final List<XMLObject> headers =
-                SOAPSupport.getInboundHeaderBlock(messageContext, ChannelBindings.DEFAULT_ELEMENT_NAME, null, true);
+        final List<XMLObject> headers = SOAPSupport.getInboundHeaderBlock(messageContext,
+                ChannelBindings.DEFAULT_ELEMENT_NAME, null, finalDestination);
         for (final XMLObject header : headers) {
             if (header instanceof ChannelBindings) {
-                channelBindings.add((ChannelBindings) header);
+                if (null == ((ActorBearing) header).getSOAP11Actor() || nextDestination) {
+                    channelBindings.add((ChannelBindings) header);
+                }
             }
         }
         
