e77e9b023091d29874f6d8884549bac5778af1c9
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2014-07-15 04:17:56 +0000

    IDP-414: Add ChannelBindings context class.

---
+++ opensaml-saml-api/src/main/java/org/opensaml/saml/common/messaging/context/ChannelBindingsContext.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.saml.common.messaging.context;
+
+import java.util.Collection;
+
+import javax.annotation.Nonnull;
+
+import net.shibboleth.utilities.java.support.annotation.constraint.Live;
+import net.shibboleth.utilities.java.support.annotation.constraint.NonnullElements;
+
+import org.opensaml.messaging.context.BaseContext;
+import org.opensaml.saml.ext.saml2cb.ChannelBindings;
+
+import com.google.common.collect.Lists;
+
+/**
+ * Context, usually attached to a {@link org.opensaml.messaging.context.MessageContext}
+ * that carries a collection of {@link ChannelBindings} objects supplied with a message.
+ */
+public class ChannelBindingsContext extends BaseContext {
+
+    /** The AttributeConsumingService. */
+    @Nonnull @NonnullElements private Collection<ChannelBindings> channelBindings;
+    
+    /** Constructor. */
+    public ChannelBindingsContext() {
+        channelBindings = Lists.newArrayList();
+    }
+
+    /**
+     * Gets the assertion to be validated.
+     * 
+     * @return the assertion to be validated
+     */
+    @Nonnull @NonnullElements @Live public Collection<ChannelBindings> getChannelBindings() {
+        return channelBindings;
+    }
+    
+}
