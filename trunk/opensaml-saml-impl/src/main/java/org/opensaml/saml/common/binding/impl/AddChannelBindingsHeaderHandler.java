0b2b4c7a88d647a5e8e4ae2306743880c979989b
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2014-07-16 02:54:09 +0000

    IDP-414: Handler to add ChannelBindings to SOAP envelope.

---
+++ trunk/opensaml-saml-impl/src/main/java/org/opensaml/saml/common/binding/impl/AddChannelBindingsHeaderHandler.java
@@ -0,0 +1,117 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.saml.common.binding.impl;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import net.shibboleth.utilities.java.support.component.ComponentSupport;
+import net.shibboleth.utilities.java.support.logic.Constraint;
+
+import org.opensaml.core.xml.config.XMLObjectProviderRegistrySupport;
+import org.opensaml.messaging.context.MessageContext;
+import org.opensaml.messaging.context.navigate.ChildContextLookup;
+import org.opensaml.messaging.handler.AbstractMessageHandler;
+import org.opensaml.messaging.handler.MessageHandlerException;
+import org.opensaml.saml.common.SAMLObjectBuilder;
+import org.opensaml.saml.common.messaging.context.ChannelBindingsContext;
+import org.opensaml.saml.ext.saml2cb.ChannelBindings;
+import org.opensaml.soap.messaging.context.SOAP11Context;
+import org.opensaml.soap.soap11.ActorBearing;
+import org.opensaml.soap.util.SOAPSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Function;
+import com.google.common.base.Functions;
+
+/**
+ * MessageHandler to add {@link ChannelBindings} headers to an outgoing SOAP envelope.
+ */
+public class AddChannelBindingsHeaderHandler extends AbstractMessageHandler {
+   
+    /** Class logger. */
+    @Nonnull private final Logger log = LoggerFactory.getLogger(AddChannelBindingsHeaderHandler.class);
+
+    /** Builder for ChannelBindings header. */
+    @Nonnull private final SAMLObjectBuilder<ChannelBindings> cbBuilder;
+    
+    /** Strategy used to locate the {@link ChannelBindingsContext} to operate on. */
+    @Nonnull private Function<MessageContext,ChannelBindingsContext> channelBindingsContextLookupStrategy;
+    
+    /** The ChannelBindingsContext to operate on. */
+    @Nullable private ChannelBindingsContext channelBindingsContext;
+    
+    /** Constructor. */
+    public AddChannelBindingsHeaderHandler() {
+        cbBuilder = (SAMLObjectBuilder<ChannelBindings>)
+                XMLObjectProviderRegistrySupport.getBuilderFactory().<ChannelBindings>getBuilderOrThrow(
+                        ChannelBindings.DEFAULT_ELEMENT_NAME);
+        channelBindingsContextLookupStrategy =
+                Functions.compose(new ChildContextLookup<>(ChannelBindingsContext.class),
+                        new ChildContextLookup<MessageContext,SOAP11Context>(SOAP11Context.class));
+    }
+    
+    /**
+     * Set the strategy used to locate the {@link ChannelBindingsContext} to operate on.
+     * 
+     * @param strategy lookup strategy
+     */
+    public void setChannelBindingsContextLookupStrategy(
+            @Nonnull final Function<MessageContext,ChannelBindingsContext> strategy) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+
+        channelBindingsContextLookupStrategy = Constraint.isNotNull(strategy,
+                "ChannelBindingsContext lookup strategy cannot be null");
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    protected boolean doPreInvoke(@Nonnull final MessageContext messageContext) throws MessageHandlerException {
+        
+        if (!super.doPreInvoke(messageContext)) {
+            return false;
+        }
+        
+        channelBindingsContext = channelBindingsContextLookupStrategy.apply(messageContext);
+        if (channelBindingsContext == null || channelBindingsContext.getChannelBindings().isEmpty()) {
+            log.debug("{} No ChannelBindings to add, nothing to do", getLogPrefix());
+            return false;
+        }
+        
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void doInvoke(@Nonnull final MessageContext messageContext) throws MessageHandlerException {
+
+        for (final ChannelBindings cb : channelBindingsContext.getChannelBindings()) {
+            final ChannelBindings header = cbBuilder.buildObject();
+            header.setType(cb.getType());
+            SOAPSupport.addSOAP11MustUnderstandAttribute(header, true);
+            SOAPSupport.addSOAP11ActorAttribute(header, ActorBearing.SOAP11_ACTOR_NEXT);
+            try {
+                SOAPSupport.addHeaderBlock(messageContext, header);
+            } catch (final Exception e) {
+                throw new MessageHandlerException(e);
+            }
+        }
+    }
+    
+}
---
+++ trunk/opensaml-saml-impl/src/test/java/org/opensaml/saml/common/binding/impl/AddChannelBindingsHeaderHandlerTest.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.saml.common.binding.impl;
+
+import java.util.List;
+
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+
+import org.opensaml.core.OpenSAMLInitBaseTestCase;
+import org.opensaml.core.xml.XMLObject;
+import org.opensaml.core.xml.config.XMLObjectProviderRegistrySupport;
+import org.opensaml.messaging.context.MessageContext;
+import org.opensaml.messaging.handler.MessageHandlerException;
+import org.opensaml.saml.common.SAMLObjectBuilder;
+import org.opensaml.saml.common.messaging.context.ChannelBindingsContext;
+import org.opensaml.saml.ext.saml2cb.ChannelBindings;
+import org.opensaml.saml.saml2.ecp.Response;
+import org.opensaml.soap.messaging.context.SOAP11Context;
+import org.opensaml.soap.soap11.Envelope;
+import org.opensaml.soap.util.SOAPSupport;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/** {@link AddChannelBindingsHeaderHandler} unit test. */
+public class AddChannelBindingsHeaderHandlerTest extends OpenSAMLInitBaseTestCase {
+    
+    private ChannelBindingsContext cbc;
+    
+    private MessageContext<Response> messageCtx;
+    
+    private AddChannelBindingsHeaderHandler handler;
+    
+    @BeforeMethod public void setUp() throws ComponentInitializationException {
+        messageCtx = new MessageContext<>();
+        
+        final ChannelBindings cb = ((SAMLObjectBuilder<ChannelBindings>)
+                XMLObjectProviderRegistrySupport.getBuilderFactory().<ChannelBindings>getBuilderOrThrow(
+                        ChannelBindings.DEFAULT_ELEMENT_NAME)).buildObject();
+        cb.setType("foo");
+        cbc = new ChannelBindingsContext();
+        cbc.getChannelBindings().add(cb);
+        
+        handler = new AddChannelBindingsHeaderHandler();
+        handler.initialize();
+    }
+    
+    /** Test that the handler does nothing on a missing CB context. */
+    @Test public void testNoBindings() throws MessageHandlerException {
+        
+        handler.invoke(messageCtx);
+        
+        final List<XMLObject> headers =
+                SOAPSupport.getInboundHeaderBlock(messageCtx, ChannelBindings.DEFAULT_ELEMENT_NAME, null, true);
+        
+        Assert.assertTrue(headers.isEmpty());
+    }
+    
+    /** Test that the handler errors on a missing SOAP context. */
+    @Test(expectedExceptions=MessageHandlerException.class)
+    public void testMissingEnvelope() throws MessageHandlerException, ComponentInitializationException {
+        messageCtx.getSubcontext(SOAP11Context.class, true).addSubcontext(cbc);
+
+        handler.invoke(messageCtx);
+    }
+
+    /** Test that the handler works. */
+    @Test public void testSuccess() throws MessageHandlerException, ComponentInitializationException {
+        final Envelope env = XMLObjectProviderRegistrySupport.getBuilderFactory().<Envelope>getBuilderOrThrow(
+                Envelope.DEFAULT_ELEMENT_NAME).buildObject(Envelope.DEFAULT_ELEMENT_NAME);
+        messageCtx.getSubcontext(SOAP11Context.class, true).setEnvelope(env);
+        messageCtx.getSubcontext(SOAP11Context.class).addSubcontext(cbc);
+        
+        handler.invoke(messageCtx);
+        
+        final List<XMLObject> headers =
+                SOAPSupport.getInboundHeaderBlock(messageCtx, ChannelBindings.DEFAULT_ELEMENT_NAME, null, true);
+        
+        Assert.assertEquals(headers.size(), 1);
+        Assert.assertEquals(((ChannelBindings) headers.get(0)).getType(), "foo");
+    }
+    
+}
