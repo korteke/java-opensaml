11ee5b6d6a56917f813f3219461107550e0a544d
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2014-09-20 04:26:55 +0000

    Port down entity group predicate for broader use.

---
+++ opensaml-saml-impl/src/main/java/org/opensaml/saml/common/profile/logic/EntityGroupNamePredicate.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development, 
+ * Inc. (UCAID) under one or more contributor license agreements.  See the 
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache 
+ * License, Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.saml.common.profile.logic;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import net.shibboleth.utilities.java.support.annotation.constraint.NonnullElements;
+import net.shibboleth.utilities.java.support.primitive.StringSupport;
+
+import org.opensaml.saml.metadata.EntityGroupName;
+import org.opensaml.saml.saml2.metadata.EntityDescriptor;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Sets;
+
+/**
+ * Predicate to determine whether one of a set of names matches any of an entity's containing
+ * {@link org.opensaml.saml.saml2.metadata.EntitiesDescriptor} groups. 
+ */
+public class EntityGroupNamePredicate implements Predicate<EntityDescriptor> {
+    
+    /** Error events to handle locally, even if possible to do so with a response. */
+    @Nonnull @NonnullElements private Set<String> groupNames;
+    
+    /** Constructor. */
+    public EntityGroupNamePredicate() {
+        groupNames = Collections.emptySet();
+    }
+    
+    /**
+     * Set the group names to look for.
+     * 
+     * @param names the group names to look for
+     */
+    public void setGroupNames(@Nonnull @NonnullElements final Collection<String> names) {
+        if (names == null) {
+            groupNames = Collections.emptySet();
+        } else {
+            groupNames = Sets.newHashSetWithExpectedSize(names.size());
+            for (final String name : names) {
+                final String trimmed = StringSupport.trimOrNull(name);
+                if (trimmed != null) {
+                    groupNames.add(trimmed);
+                }
+            }
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean apply(@Nullable final EntityDescriptor input) {
+        if (input != null) {
+            for (final EntityGroupName group : input.getObjectMetadata().get(EntityGroupName.class)) {
+                if (groupNames.contains(group.getName())) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+
+}
