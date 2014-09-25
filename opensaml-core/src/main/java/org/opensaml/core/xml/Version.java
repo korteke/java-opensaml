2ac30b23321f5ac31c88f3674058c31a9adf86fb
putmanb <putmanb@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2014-09-25 23:45:36 +0000

    OSJ-91: org.opensaml.core.xml.Version is currently broken

--- opensaml-core/src/main/java/org/opensaml/core/xml/Version.java
+++ opensaml-core/src/main/java/org/opensaml/core/xml/Version.java
@@ -17,23 +17,13 @@
 
 package org.opensaml.core.xml;
 
+import javax.annotation.Nullable;
+
 /** Class for printing the version of this library. */
 public final class Version {
 
-    /** Name of the library. */
-    private static final String NAME;
-
-    /** Library version. */
-    private static final String VERSION;
-
-    /** Library major version number. */
-    private static final int MAJOR_VERSION;
-
-    /** Library minor version number. */
-    private static final int MINOR_VERSION;
-
-    /** Library micro version number. */
-    private static final int MICRO_VERSION;
+    /** IdP version. */
+    @Nullable private static final String VERSION = Version.class.getPackage().getImplementationVersion();
 
     /** Constructor. */
     private Version() {
@@ -45,61 +35,16 @@ public final class Version {
      * @param args command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(NAME + " version " + VERSION);
+        System.out.println(VERSION);
     }
 
     /**
-     * Gets the name of the library.
-     * 
-     * @return name of the library
-     */
-    public static String getName() {
-        return NAME;
-    }
-
-    /**
-     * Gets the version of the library.
+     * Get the version of the library.
      * 
      * @return version of the library
      */
-    public static String getVersion() {
+    @Nullable public static String getVersion() {
         return VERSION;
     }
-
-    /**
-     * Gets the major version number of the library.
-     * 
-     * @return major version number of the library
-     */
-    public static int getMajorVersion() {
-        return MAJOR_VERSION;
-    }
-
-    /**
-     * Gets the minor version number of the library.
-     * 
-     * @return minor version number of the library
-     */
-    public static int getMinorVersion() {
-        return MINOR_VERSION;
-    }
-
-    /**
-     * Gets the micro version number of the library.
-     * 
-     * @return micro version number of the library
-     */
-    public static int getMicroVersion() {
-        return MICRO_VERSION;
-    }
-
-    static {
-        Package pkg = Version.class.getPackage();
-        NAME = pkg.getImplementationTitle().intern();
-        VERSION = pkg.getImplementationVersion().intern();
-        String[] versionParts = VERSION.split("\\.");
-        MAJOR_VERSION = Integer.parseInt(versionParts[0]);
-        MINOR_VERSION = Integer.parseInt(versionParts[1]);
-        MICRO_VERSION = Integer.parseInt(versionParts[2]);
-    }
+    
 }
