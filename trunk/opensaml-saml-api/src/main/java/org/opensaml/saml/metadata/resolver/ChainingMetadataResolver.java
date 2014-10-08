5259ba4c7e741aef4548b822a765beb51a2a0e52
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2014-10-08 03:30:57 +0000

    IDP-72 - Action / unit test to refresh selected metadata source from inside service. Moved ChainingMetadataResolver into saml-api to enable access to the wrapped resolvers.

--- trunk/opensaml-saml-api/src/main/java/org/opensaml/saml/metadata/resolver/ChainingMetadataResolver.java
+++ trunk/opensaml-saml-api/src/main/java/org/opensaml/saml/metadata/resolver/ChainingMetadataResolver.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.opensaml.saml.metadata.resolver.impl;
+package org.opensaml.saml.metadata.resolver;
 
 import java.util.Collections;
 import java.util.Iterator;
@@ -24,58 +24,63 @@ import java.util.List;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import net.shibboleth.utilities.java.support.annotation.constraint.NonnullElements;
+import net.shibboleth.utilities.java.support.annotation.constraint.NotLive;
+import net.shibboleth.utilities.java.support.annotation.constraint.Unmodifiable;
 import net.shibboleth.utilities.java.support.component.AbstractIdentifiableInitializableComponent;
 import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
 import net.shibboleth.utilities.java.support.component.ComponentSupport;
 import net.shibboleth.utilities.java.support.resolver.CriteriaSet;
 import net.shibboleth.utilities.java.support.resolver.ResolverException;
 
-import org.opensaml.saml.metadata.resolver.MetadataResolver;
-import org.opensaml.saml.metadata.resolver.RefreshableMetadataResolver;
 import org.opensaml.saml.metadata.resolver.filter.MetadataFilter;
 import org.opensaml.saml.saml2.metadata.EntityDescriptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 
 /**
  * A metadata provider that uses registered resolvers, in turn, to answer queries.
  * 
  * The Iterable of entity descriptors returned is the first non-null and non-empty Iterable found while iterating over
  * the registered resolvers in resolver list order.
- * 
  */
 public class ChainingMetadataResolver extends AbstractIdentifiableInitializableComponent implements MetadataResolver,
         RefreshableMetadataResolver {
 
     /** Class logger. */
-    private final Logger log = LoggerFactory.getLogger(ChainingMetadataResolver.class);
+    @Nonnull private final Logger log = LoggerFactory.getLogger(ChainingMetadataResolver.class);
 
     /** Registered resolvers. */
-    private List<MetadataResolver> resolvers;
+    @Nonnull @NonnullElements private List<MetadataResolver> resolvers;
 
     /** Constructor. */
     public ChainingMetadataResolver() {
-        super();
-        resolvers = Collections.EMPTY_LIST;
+        resolvers = Collections.emptyList();
     }
 
     /**
-     * Gets an immutable the list of currently registered resolvers.
+     * Get an immutable the list of currently registered resolvers.
      * 
      * @return list of currently registered resolvers
      */
-    public List<MetadataResolver> getResolvers() {
-        return resolvers;
+    @Nonnull @NonnullElements @Unmodifiable @NotLive public List<MetadataResolver> getResolvers() {
+        return ImmutableList.copyOf(resolvers);
     }
 
     /**
-     * Sets the current set of metadata resolvers.
+     * Set the registered metadata resolvers.
      * 
      * @param newResolvers the metadata resolvers to use
      * 
-     * @throws ResolverException thrown if there is a problem adding the metadata provider
+     * @throws ResolverException thrown if there is a problem adding the metadata resolvers
      */
-    public void setResolvers(List<MetadataResolver> newResolvers) throws ResolverException {
+    public void setResolvers(@Nonnull @NonnullElements final List<MetadataResolver> newResolvers)
+            throws ResolverException {
         ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
         ComponentSupport.ifDestroyedThrowDestroyedComponentException(this);
 
@@ -84,7 +89,8 @@ public class ChainingMetadataResolver extends AbstractIdentifiableInitializableC
             return;
         }
 
-        resolvers = Collections.unmodifiableList(newResolvers);
+        resolvers = Lists.newArrayListWithExpectedSize(newResolvers.size());
+        resolvers.addAll(Collections2.filter(newResolvers, Predicates.notNull()));
     }
 
     /** {@inheritDoc} */
@@ -95,7 +101,7 @@ public class ChainingMetadataResolver extends AbstractIdentifiableInitializableC
 
     /** {@inheritDoc} */
     @Override public void setRequireValidMetadata(boolean requireValidMetadata) {
-        throw new UnsupportedOperationException("Setting require valid metadata is not supported on chaining resolver");
+        throw new UnsupportedOperationException("Setting requireValidMetadata is not supported on chaining resolver");
     }
 
     /** {@inheritDoc} */
@@ -110,12 +116,13 @@ public class ChainingMetadataResolver extends AbstractIdentifiableInitializableC
     }
 
     /** {@inheritDoc} */
-    @Override @Nullable public EntityDescriptor resolveSingle(CriteriaSet criteria) throws ResolverException {
+    @Override
+    @Nullable public EntityDescriptor resolveSingle(@Nullable final CriteriaSet criteria) throws ResolverException {
         ComponentSupport.ifNotInitializedThrowUninitializedComponentException(this);
 
-        Iterable<EntityDescriptor> iterable = resolve(criteria);
+        final Iterable<EntityDescriptor> iterable = resolve(criteria);
         if (iterable != null) {
-            Iterator<EntityDescriptor> iterator = iterable.iterator();
+            final Iterator<EntityDescriptor> iterator = iterable.iterator();
             if (iterator != null && iterator.hasNext()) {
                 return iterator.next();
             }
@@ -124,18 +131,19 @@ public class ChainingMetadataResolver extends AbstractIdentifiableInitializableC
     }
 
     /** {@inheritDoc} */
-    @Override @Nonnull public Iterable<EntityDescriptor> resolve(CriteriaSet criteria) throws ResolverException {
+    @Override
+    @Nonnull public Iterable<EntityDescriptor> resolve(@Nullable final CriteriaSet criteria) throws ResolverException {
         ComponentSupport.ifNotInitializedThrowUninitializedComponentException(this);
 
-        for (MetadataResolver resolver : resolvers) {
+        for (final MetadataResolver resolver : resolvers) {
             try {
-                Iterable<EntityDescriptor> descriptors = resolver.resolve(criteria);
+                final Iterable<EntityDescriptor> descriptors = resolver.resolve(criteria);
                 if (descriptors != null && descriptors.iterator().hasNext()) {
                     return descriptors;
                 }
-            } catch (ResolverException e) {
-                log.warn("Error retrieving metadata from resolver of type {}, proceeding to next resolver", resolver
-                        .getClass().getName(), e);
+            } catch (final ResolverException e) {
+                log.warn("Error retrieving metadata from resolver of type {}, proceeding to next resolver",
+                        resolver.getClass().getName(), e);
                 continue;
             }
         }
@@ -145,7 +153,7 @@ public class ChainingMetadataResolver extends AbstractIdentifiableInitializableC
 
     /** {@inheritDoc} */
     @Override public void refresh() throws ResolverException {
-        for (MetadataResolver resolver : resolvers) {
+        for (final MetadataResolver resolver : resolvers) {
             if (resolver instanceof RefreshableMetadataResolver) {
                 ((RefreshableMetadataResolver) resolver).refresh();
             }
