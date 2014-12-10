75c167164cfda8a53ae96b33102093b450675488
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2014-12-10 04:40:01 +0000

    IDP-76 - Treat port 443 as "insecure" for SAML messaging purposes

--- opensaml-profile-impl/src/main/java/org/opensaml/profile/action/impl/HttpServletRequestMessageChannelSecurity.java
+++ opensaml-profile-impl/src/main/java/org/opensaml/profile/action/impl/HttpServletRequestMessageChannelSecurity.java
@@ -17,7 +17,10 @@
 
 package org.opensaml.profile.action.impl;
 
+import javax.servlet.http.HttpServletRequest;
+
 import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+import net.shibboleth.utilities.java.support.component.ComponentSupport;
 
 import org.opensaml.messaging.context.MessageChannelSecurityContext;
 import org.opensaml.profile.context.ProfileRequestContext;
@@ -28,6 +31,32 @@ import org.opensaml.profile.context.ProfileRequestContext;
  */
 public class HttpServletRequestMessageChannelSecurity extends AbstractMessageChannelSecurity {
 
+    /** Flag controlling whether traffic on the default TLS port is "secure". */
+    private boolean defaultPortInsecure;
+    
+    /** Constructor. */
+    public HttpServletRequestMessageChannelSecurity() {
+        defaultPortInsecure = true;
+    }
+    
+    /**
+     * Set whether traffic on the default TLS port is "secure" for the purposes of this action.
+     * 
+     * <p>Defaults to "true"</p>
+     *
+     * <p>Ordinarily TLS is considered a "secure" channel, but traffic to a default port meant
+     * for browser access tends to rely on server certificates that are unsuited to secure messaging
+     * use cases. This flag allows software layers to recognize traffic on this port as "insecure" and
+     * needing additional security measures.</p>
+     * 
+     * @param flag flag to set
+     */
+    public void setDefaultPortInsecure(final boolean flag) {
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+        
+        defaultPortInsecure = flag;
+    }
+    
     /** {@inheritDoc} */
     @Override
     protected void doInitialize() throws ComponentInitializationException {
@@ -42,8 +71,15 @@ public class HttpServletRequestMessageChannelSecurity extends AbstractMessageCha
     protected void doExecute(ProfileRequestContext profileRequestContext) {
         final MessageChannelSecurityContext channelContext =
                 getParentContext().getSubcontext(MessageChannelSecurityContext.class, true);
-        channelContext.setConfidentialityActive(getHttpServletRequest().isSecure());
-        channelContext.setIntegrityActive(getHttpServletRequest().isSecure());
+        
+        final HttpServletRequest request = getHttpServletRequest();
+        if (request.isSecure() && (!defaultPortInsecure || request.getLocalPort() != 443)) {
+            channelContext.setConfidentialityActive(true);
+            channelContext.setIntegrityActive(true);
+        } else {
+            channelContext.setConfidentialityActive(false);
+            channelContext.setIntegrityActive(false);
+        }
     }
 
 }
