5d22afa94bf410a34191ce63139313f642fef0e2
scantor <scantor@b8276f27-d41f-4ffb-8289-9a0d50c8392a>
2014-12-16 02:33:32 +0000

    Fix log message.

--- trunk/opensaml-security-impl/src/main/java/org/opensaml/security/x509/impl/PKIXX509CredentialTrustEngine.java
+++ trunk/opensaml-security-impl/src/main/java/org/opensaml/security/x509/impl/PKIXX509CredentialTrustEngine.java
@@ -205,7 +205,7 @@ public class PKIXX509CredentialTrustEngine implements PKIXTrustEngine<X509Creden
             log.debug("No credential name evaluator was available, skipping trusted name evaluation");
            return true; 
         } else if (trustedNames == null) {
-            log.debug("Truted names was null, signalling PKIX resolver does not support trusted names resolution, " 
+            log.debug("Trusted names was null, signalling PKIX resolver does not support trusted names resolution, " 
                     + "skipping trusted name evaluation");
            return true; 
         } else {
